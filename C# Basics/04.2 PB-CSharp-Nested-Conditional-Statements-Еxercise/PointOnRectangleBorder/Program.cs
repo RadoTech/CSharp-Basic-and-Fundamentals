994cb4e8febb4cc24b74de3aff4be3672d6caa8a
RADO <53035214+RadoTech@users.noreply.github.com>
2019-10-02 20:07:52 +0300

    Add files via upload

---
+++ C# Basics/04.2 PB-CSharp-Nested-Conditional-Statements-Еxercise/PointOnRectangleBorder/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace PointOnRectangleBorder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //input
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+            double x = double.Parse(Console.ReadLine());
+            double y = double.Parse(Console.ReadLine());
+
+            //logic
+            //x1 < x2 && y1 <y2
+
+            bool firstCondition = (x == x1 || x == x2) && (y >= y1 && y <= y2);
+            bool secondCondition = (y == y1 || y == y2) && (x >= x1 && x <= x2);
+
+            if (firstCondition || secondCondition)
+            {
+                Console.WriteLine("Border");
+            }
+            else
+            {
+                Console.WriteLine("Inside / Outside");
+            }
+
+
+        }
+    }
+}
