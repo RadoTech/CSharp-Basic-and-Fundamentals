2cf1900222eddd27b3f1a8ffd25ce2f047e3394e
RADO <53035214+RadoTech@users.noreply.github.com>
2019-10-07 13:38:07 +0300

    Add files via upload

---
+++ C# Basics/04.2 PB-CSharp-Nested-Conditional-Statements-Еxercise/Volleyball/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Volleyball
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //input
+            string typeOfYear = Console.ReadLine();
+            double bankHolidaysCount = double.Parse(Console.ReadLine());
+            double weekendsHome = double.Parse(Console.ReadLine());
+
+            //logic
+
+            double weekendsPerYear = 48;
+            double bankHolidaysPlay = (bankHolidaysCount / 3) * 2;
+
+            double weekendsHomePlay = weekendsHome;
+
+            double weekendsSofia = weekendsPerYear - weekendsHome;
+            double weekendsSofiaFree = (weekendsSofia / 4) * 3;
+            double weekendsSofiaPlay = weekendsSofiaFree;
+
+            double leapYearRate = 1.15;
+
+            double VolleyballDays = 0;
+
+            if (typeOfYear == "leap")
+            {
+                VolleyballDays = ((bankHolidaysPlay + weekendsHomePlay + weekendsSofiaPlay) * leapYearRate);
+            }
+            else if (typeOfYear == "normal")
+            {
+                VolleyballDays = bankHolidaysPlay + weekendsHomePlay + weekendsSofiaPlay;
+            }
+            //output
+
+
+
+            Console.WriteLine($"{Math.Floor(VolleyballDays)}");
+
+
+        }
+    }
+}
