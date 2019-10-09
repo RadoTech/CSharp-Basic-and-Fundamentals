9e6de2d96d335fe20a3f948561edbd9abde4d698
RADO <53035214+RadoTech@users.noreply.github.com>
2019-10-09 16:50:23 +0300

    Add files via upload

---
+++ C# Basics/05.1 PB-CSharp-While-Loop-Lab/Account Balance/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace AccountBalance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int installments = int.Parse(Console.ReadLine());
+            int firstInstallment = 1;
+            double sum = 0.00;
+            double totalSum = 0.00;
+
+            while (firstInstallment <= installments)
+            {
+                sum = double.Parse(Console.ReadLine());
+
+                if (sum < 0)
+                {
+                    Console.WriteLine("Invalid operation!");
+                    Console.WriteLine($"Total: {totalSum:f2}");
+                    break;
+                }
+
+                totalSum += sum;
+                Console.WriteLine($"Increase: {sum:f2}");
+                firstInstallment++;
+
+                if (firstInstallment > installments)
+                {
+                    Console.WriteLine($"Total: {totalSum:f2}");
+                }
+            }
+        }
+    }
+}
