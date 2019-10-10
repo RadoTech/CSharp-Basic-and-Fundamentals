9a19ae5989c8c84567370c5f265bdf9e125ee8c9
RADO <53035214+RadoTech@users.noreply.github.com>
2019-10-10 10:03:11 +0300

    Add files via upload

---
+++ C# Basics/05.1 PB-CSharp-While-Loop-Lab/Sequence 2k+1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Sequence2k_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //input
+            int number = int.Parse(Console.ReadLine());
+
+            //logic
+
+            int n = 1;
+
+            Console.WriteLine(n);
+
+            while (n <= number)
+            {
+                n = (n * 2) + 1;
+
+                if (n <= number)
+                {
+
+                    Console.WriteLine(n);
+                }
+
+
+            }
+
+
+        }
+    }
+}
