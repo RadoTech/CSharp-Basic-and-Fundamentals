46381094ab5bd2dab283c1a2884ef131aba5530b
RADO <53035214+RadoTech@users.noreply.github.com>
2019-10-10 11:59:55 +0300

    Add files via upload

---
+++ C# Basics/05.1 PB-CSharp-While-Loop-Lab/Graduation pt.2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace GraduationPt2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //input
+
+            string studentName = Console.ReadLine();
+
+            //logic
+            int year = 0;
+            int yearAll = 12;
+            double grade = 0.00;
+            double totalGradeSum = 0.00;
+            double totalGrade = 0.00;
+            double gradePass = 4.00;
+            int failCount = 0;
+            int failCountYear = 0;
+
+            while (year <= yearAll)
+            {
+                grade = double.Parse(Console.ReadLine());
+
+                if (grade >= gradePass)
+                {
+                    year++;
+                    totalGradeSum += grade;
+                }
+                else if (grade < gradePass)
+                {
+                    failCount++;
+                    if (failCount == 2)
+                    {
+                        failCountYear = ++year;
+                        Console.WriteLine($"{studentName} has been excluded at {failCountYear} grade");
+                        break;
+                    }
+                }
+
+                if (year == yearAll)
+                {
+                    totalGrade = (totalGradeSum / yearAll);
+                    Console.WriteLine($"{studentName} graduated. Average grade: {totalGrade:f2}");
+                    break;
+                }
+            }
+        }
+    }
+}
