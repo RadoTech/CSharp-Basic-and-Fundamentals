1af177d13fa1eb5b8d82b27cf327c0b9f9a12092
RADO <53035214+RadoTech@users.noreply.github.com>
2019-10-10 10:01:33 +0300

    Add files via upload

---
+++ C# Basics/05.1 PB-CSharp-While-Loop-Lab/Password/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Password
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nickname = Console.ReadLine();
+            string passwordSet = Console.ReadLine();
+
+            string enterPassword = Console.ReadLine();
+            
+            while (enterPassword != passwordSet )
+            {
+                enterPassword = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Welcome {nickname}!");
+        }
+    }
+}
