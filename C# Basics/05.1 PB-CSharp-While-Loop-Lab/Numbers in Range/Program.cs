1980db69ef5f3ad1e5dcfe2a61a0f086eb55d28c
RADO <53035214+RadoTech@users.noreply.github.com>
2019-10-10 14:46:33 +0300

    Add files via upload

---
+++ C# Basics/05.1 PB-CSharp-While-Loop-Lab/Numbers in Range/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace NumbersInRange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //input
+            int number = int.Parse(Console.ReadLine());
+
+            //logic
+
+            while (number < 1 || number > 100)
+            {
+                Console.WriteLine("Invalid number!");
+                number = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine($"The number is: {number}");
+        }
+    }
+}
