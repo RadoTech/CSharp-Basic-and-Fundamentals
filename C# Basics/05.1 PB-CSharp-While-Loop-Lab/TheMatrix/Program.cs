02389af92844b12a8c73a931e0ad5e3ae8fb0afa
RADO <53035214+RadoTech@users.noreply.github.com>
2019-10-10 09:58:59 +0300

    Add files via upload

---
+++ C# Basics/05.1 PB-CSharp-While-Loop-Lab/TheMatrix/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TheMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("101");
+            }
+        }
+    }
+}
