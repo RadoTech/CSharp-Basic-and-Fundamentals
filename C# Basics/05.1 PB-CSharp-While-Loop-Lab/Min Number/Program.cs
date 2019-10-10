696cb3e76768742d5e817e61334d02b218010c98
RADO <53035214+RadoTech@users.noreply.github.com>
2019-10-10 11:04:55 +0300

    Add files via upload

---
+++ C# Basics/05.1 PB-CSharp-While-Loop-Lab/Min Number/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MinNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            int count = 1;
+            int n = 0;
+            int minNumber = int.MaxValue;
+
+            while (count <= number)
+            {
+                n = int.Parse(Console.ReadLine());
+
+                if (n < minNumber)
+                {
+                    minNumber = n;
+                }
+
+                count++;
+            }
+            Console.WriteLine(minNumber);
+        }
+    }
+}
