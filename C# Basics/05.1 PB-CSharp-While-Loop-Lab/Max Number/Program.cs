5efef7a4edef7b13d72406982b451283ec647f57
RADO <53035214+RadoTech@users.noreply.github.com>
2019-10-10 10:29:58 +0300

    Add files via upload

---
+++ C# Basics/05.1 PB-CSharp-While-Loop-Lab/Max Number/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MaxNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            int count = 1;
+            int n = 0;
+            int maxNumber = int.MinValue;
+
+            while (number >= count)
+            {
+                n = int.Parse(Console.ReadLine());
+                if (n > maxNumber)
+                {
+                    maxNumber = n;
+                }
+                count++;
+
+            }
+            Console.WriteLine(maxNumber);
+
+        }
+    }
+}
