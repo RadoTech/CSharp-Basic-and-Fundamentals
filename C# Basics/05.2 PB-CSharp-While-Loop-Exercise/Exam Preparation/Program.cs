c3bc4379057f36a4ac88794c25cdd7f29c96849f
RADO <53035214+RadoTech@users.noreply.github.com>
2019-10-13 23:20:54 +0300

    Add files via upload

---
+++ C# Basics/05.2 PB-CSharp-While-Loop-Exercise/Exam Preparation/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace ExamPreparation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int badGrades = int.Parse(Console.ReadLine());
+
+            int badGradesCount = 0;
+
+            string problemName = "";
+            string lastProblem = "";
+            int grade = 0;
+            double gradesSum = 0;
+            int problemCount = 0;
+            double averageGrade = 0;
+
+            while (!(problemName == "Enough"))
+            {
+                lastProblem = problemName;
+                
+                if (badGradesCount == badGrades)
+                {
+                    Console.WriteLine($"You need a break, {badGrades} poor grades.");
+                    return;
+
+                }
+                problemName = Console.ReadLine();
+
+                if (problemName == "Enough")
+                {
+                    break;
+                }
+
+                grade = int.Parse(Console.ReadLine());
+                if (grade <= 4)
+                {
+                    ++badGradesCount;
+                }
+                
+                gradesSum += grade;
+                problemCount++;
+
+            }
+
+            if (problemName == "Enough")
+            {
+                averageGrade = gradesSum / problemCount;
+
+                Console.WriteLine($"Average score: {averageGrade:f2}");
+                Console.WriteLine($"Number of problems: {problemCount}");
+                Console.WriteLine($"Last problem: {lastProblem}");
+
+            }
+
+
+        }
+    }
+}
