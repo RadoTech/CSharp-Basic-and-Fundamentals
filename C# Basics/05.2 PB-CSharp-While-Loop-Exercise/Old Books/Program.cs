288e8eca1cad3c210af0a0bc7751c3ac052b1634
RADO <53035214+RadoTech@users.noreply.github.com>
2019-10-13 22:26:32 +0300

    Add files via upload

---
+++ C# Basics/05.2 PB-CSharp-While-Loop-Exercise/Old Books/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace OldBooks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string bookSearch = Console.ReadLine();
+            int bookCapacity = int.Parse(Console.ReadLine());
+
+            string bookNext = "";
+            int bookCount = 0;
+            
+
+            while (bookCount <= bookCapacity)
+            {
+                
+                if (bookSearch == bookNext)
+                {
+                    Console.WriteLine($"You checked {bookCount-1} books and found it.");
+                    return;
+                }
+                bookCount++;
+                bookNext = Console.ReadLine();
+                
+            }
+            Console.WriteLine("The book you search is not here!");
+            Console.WriteLine($"You checked {bookCapacity} books.");
+        }
+    }
+}
