bf864b0316021b4cc39e80014236ce4ba3914366
RADO <53035214+RadoTech@users.noreply.github.com>
2019-10-14 14:50:59 +0300

    Add files via upload

---
+++ C# Basics/05.2 PB-CSharp-While-Loop-Exercise/Walking/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Walking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stepsPerWalk = Console.ReadLine();
+            int stepsCounter = 0;
+
+            while (stepsPerWalk != "Going home")
+            {
+                stepsCounter += int.Parse(stepsPerWalk);
+                if (stepsCounter >= 10000)
+                {
+                    Console.WriteLine("Goal reached! Good job!");
+                    return;
+                }
+                stepsPerWalk = Console.ReadLine();
+            }
+            if (stepsPerWalk == "Going home")
+            {
+                stepsPerWalk = Console.ReadLine();
+                stepsCounter += int.Parse(stepsPerWalk);
+                if (stepsCounter >= 10000)
+                {
+                    Console.WriteLine("Goal reached! Good job!");
+                }
+                else if (stepsCounter < 10000)
+                {
+                    int stepsLeft = 10000 - stepsCounter;
+                    Console.WriteLine($"{stepsLeft} more steps to reach goal.");
+                }
+            }
+        }
+    }
+}
