0a9d0610eb9de2c9700d554e9679a2a62b02713a
RADO <53035214+RadoTech@users.noreply.github.com>
2019-10-14 12:52:45 +0300

    Add files via upload

---
+++ C# Basics/05.2 PB-CSharp-While-Loop-Exercise/Vacation/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Vacation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //logic
+            double neededMoney = double.Parse(Console.ReadLine());
+            double ownedMoney = double.Parse(Console.ReadLine());
+
+            //logic
+
+            int daysCounter = 0;
+            int spendingCounter = 0;
+
+            while ((ownedMoney < neededMoney) && (spendingCounter < 5))
+            {
+                string command = Console.ReadLine();
+                double money = double.Parse(Console.ReadLine());
+                daysCounter++;
+
+                if (command == "save")
+
+                {
+                    ownedMoney += money;
+                    spendingCounter = 0;
+                }
+                else if (command == "spend")
+                {
+                    ownedMoney -= money;
+                    spendingCounter += 1;
+                    if (ownedMoney < 0)
+                    {
+                        ownedMoney = 0;
+                    }
+                }
+            }
+
+            if (spendingCounter == 5)
+            {
+                Console.WriteLine("You can't save the money.");
+                Console.WriteLine(daysCounter);
+            }
+            if (ownedMoney >= neededMoney)
+            {
+                Console.WriteLine($"You saved the money for {daysCounter} days.");
+            }
+
+
+
+
+
+        }
+    }
+}
