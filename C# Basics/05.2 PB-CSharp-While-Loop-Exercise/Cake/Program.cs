46d862b3d00907be6ec9cac7449ed511f98e4659
RADO <53035214+RadoTech@users.noreply.github.com>
2019-10-15 09:27:03 +0300

    Add files via upload

---
+++ C# Basics/05.2 PB-CSharp-While-Loop-Exercise/Cake/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Cake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //input
+            int cakeW = int.Parse(Console.ReadLine());
+            int cakeL = int.Parse(Console.ReadLine());
+
+            //logic
+
+            int cakeV = cakeW * cakeL;
+
+            string command;
+            int numberTaken = 0;
+
+            while ((command = Console.ReadLine()) != "STOP")
+            {
+                int number = int.Parse(command);
+                numberTaken += number;
+                if (numberTaken > cakeV)
+                {
+                    int numberNeeded = numberTaken - cakeV;
+                    Console.WriteLine($"No more cake left! You need {numberNeeded} pieces more.");
+                    return;
+                }
+            }
+            if (command == "STOP")
+            {
+                int numberLeft = cakeV - numberTaken;
+                Console.WriteLine($"{numberLeft} pieces are left.");
+            }
+
+        }
+    }
+}
