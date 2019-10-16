1410183e138deafa6f0cd5b033d19138e159e4ed
RADO <53035214+RadoTech@users.noreply.github.com>
2019-10-16 15:03:34 +0300

    Add files via upload

---
+++ C# Basics/06.1 PB-CSharp-For-Loop-Lab/Left and Right Sum/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace LeftAndRightSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int groups = int.Parse(Console.ReadLine());
+            int groupOneSum = 0;
+            int groupTwoSum = 0;
+
+            for (int i = 1; i <= groups * 2; i++)
+            {
+                if (i <= groups)
+                {
+                    int groupOne = int.Parse(Console.ReadLine());
+                    groupOneSum += groupOne;
+                }
+                else if (i > groups)
+                {
+                    int groupTwo = int.Parse(Console.ReadLine());
+                    groupTwoSum += groupTwo;
+                }
+            }
+            if (groupOneSum == groupTwoSum)
+            {
+                Console.WriteLine($"Yes, sum = {groupOneSum}");
+            }
+            else
+            {
+                Console.WriteLine($"No, diff = {Math.Abs(groupOneSum - groupTwoSum)}");
+            }
+
+        }
+    }
+}
