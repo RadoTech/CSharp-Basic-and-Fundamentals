6422286ae1779a1f76eb5e032f34b4c81cd0606e
RADO <53035214+RadoTech@users.noreply.github.com>
2019-10-16 14:23:58 +0300

    Add files via upload

---
+++ C# Basics/06.1 PB-CSharp-For-Loop-Lab/Number sequence/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace NumberSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbersCount = int.Parse(Console.ReadLine());
+            int currentNumber = 0;
+            int maxNumber = int.MinValue;
+            int minNumber = int.MaxValue;
+
+            for (int i = 1; i <= numbersCount; i++)
+            {
+                currentNumber = int.Parse(Console.ReadLine());
+
+                if (maxNumber < currentNumber)
+                {
+                    maxNumber = currentNumber;
+                }
+                if (minNumber > currentNumber)
+                {
+                    minNumber = currentNumber;
+                }
+            }
+            Console.WriteLine($"Max number: {maxNumber}");
+            Console.WriteLine($"Min number: {minNumber}");
+        }
+    }
+}
