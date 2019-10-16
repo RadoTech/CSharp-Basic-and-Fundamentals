b4cd7ded4d7bf65f2ddc057aec3817a0e144cc8e
RADO <53035214+RadoTech@users.noreply.github.com>
2019-10-16 12:00:29 +0300

    Add files via upload

---
+++ C# Basics/06.1 PB-CSharp-For-Loop-Lab/Sum Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbersCount = int.Parse(Console.ReadLine());
+            int numbersSum = 0;
+
+            for (int i = 1; i <= numbersCount; i++)
+            {
+
+                int currentNumber = int.Parse(Console.ReadLine());
+                numbersSum += currentNumber;
+            }
+
+            Console.WriteLine(numbersSum);
+        }
+    }
+}
