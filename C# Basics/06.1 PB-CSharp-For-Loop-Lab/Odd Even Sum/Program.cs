6f3bf474bc33fb6ebf4dd9609e34d145228cba57
RADO <53035214+RadoTech@users.noreply.github.com>
2019-10-16 15:20:52 +0300

    Add files via upload

---
+++ C# Basics/06.1 PB-CSharp-For-Loop-Lab/Odd Even Sum/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ForOddEvenSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            int evenSum = 0;
+            int oddSum = 0;
+
+            for (int i = 1; i <= number; i++)
+            {
+                int currentNumber = int.Parse(Console.ReadLine());
+                if (i % 2 == 0)
+                {
+                    evenSum += currentNumber;
+                }
+                else
+                {
+                    oddSum += currentNumber;
+                }
+            }
+            if (evenSum == oddSum)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {evenSum}");
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {Math.Abs(evenSum - oddSum)}");
+            }
+        }
+    }
+}
