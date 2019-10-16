3f9ae3d332522ecc0a09e4a7d9359a4c02d058a2
RADO <53035214+RadoTech@users.noreply.github.com>
2019-10-16 23:21:17 +0300

    Add files via upload

---
+++ C# Basics/06.1 PB-CSharp-For-Loop-Lab/Clever Lily*/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace CleverLily
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = int.Parse(Console.ReadLine());
+            double washmachine = double.Parse(Console.ReadLine());
+            int toyPrice = int.Parse(Console.ReadLine());
+            int toyCount = 0;
+            int bDayMoney = 0;
+            int stolenMoney = 0;
+            int bDayMoneySaved = 0;
+
+            for (int i = 1; i <= age; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    toyCount++;
+                }
+                else if (i % 2 == 0)
+                {
+                    bDayMoney += 10;
+                    bDayMoneySaved += bDayMoney;
+                    stolenMoney++;
+                }
+            }
+            int toyMoney = toyCount * toyPrice;
+            int savedMoney = bDayMoneySaved + toyMoney - stolenMoney;
+
+            if (savedMoney >= washmachine)
+            {
+                double restMoney = savedMoney - washmachine;
+                Console.WriteLine($"Yes! {restMoney:f2}");
+            }
+            else if (savedMoney < washmachine)
+            {
+                double neededMoney = washmachine - savedMoney;
+                Console.WriteLine($"No! {neededMoney:f2}");
+            }
+        }
+    }
+}
