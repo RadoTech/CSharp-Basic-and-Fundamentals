c079c7040c4841c656c4205d6c167dcc805f5ab9
RADO <53035214+RadoTech@users.noreply.github.com>
2019-10-20 15:52:11 +0300

    Add files via upload

---
+++ C# Basics/06.2 PB-CSharp-For-Loop-Exercise/02.Half-Sum-Element/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace HalfSumElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbersCount = int.Parse(Console.ReadLine());
+
+            int maxNumber = int.MinValue;
+            int sumNumbers = 0;
+
+            for (int i = 1; i <= numbersCount; i++)
+            {
+                int currentNumber = int.Parse(Console.ReadLine());
+
+                if (currentNumber > maxNumber)
+                {
+                    maxNumber = currentNumber;
+                }
+                sumNumbers += currentNumber;
+            }
+            if (maxNumber == (sumNumbers - maxNumber))
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {maxNumber}");
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {Math.Abs((sumNumbers-maxNumber)-maxNumber)}");
+            }
+        }
+    }
+}
