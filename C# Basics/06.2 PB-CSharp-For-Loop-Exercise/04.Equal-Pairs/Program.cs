2f5f0f1c7cbda72403a8a5e39de0e799ec8f2c3f
RADO <53035214+RadoTech@users.noreply.github.com>
2019-10-20 22:23:48 +0300

    Add files via upload

---
+++ C# Basics/06.2 PB-CSharp-For-Loop-Exercise/04.Equal-Pairs/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace EqualPairs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pairsCount = int.Parse(Console.ReadLine());
+            int sumPairs = 0;
+            string result = "N/A";
+            int maxDiff = 0;
+
+            for (int i = 1; i <= pairsCount; i++)
+            {
+                int currentPairPartOne = int.Parse(Console.ReadLine());
+                int currentPairPartTwo = int.Parse(Console.ReadLine());
+                if ((currentPairPartOne + currentPairPartTwo) == sumPairs)
+                {
+                    result = "Yes";
+                }
+                else
+                {
+                    result = "No";
+                    maxDiff = Math.Abs((currentPairPartOne + currentPairPartTwo) - sumPairs);
+                }
+                sumPairs = currentPairPartOne + currentPairPartTwo;
+            }
+            if (result == "Yes"|| pairsCount == 1)
+            {
+                Console.WriteLine($"Yes, value={sumPairs}");
+            }
+            else if (result == "No")
+            {
+                Console.WriteLine($"No, maxdiff={maxDiff}");
+            }
+        }
+    }
+}
