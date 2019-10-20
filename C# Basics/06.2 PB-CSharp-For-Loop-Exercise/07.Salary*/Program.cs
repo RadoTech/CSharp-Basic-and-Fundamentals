de279dba33a56ef63ec99435f5c164ebe85a0d42
RADO <53035214+RadoTech@users.noreply.github.com>
2019-10-20 15:01:11 +0300

    Add files via upload

---
+++ C# Basics/06.2 PB-CSharp-For-Loop-Exercise/07.Salary*/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Salary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int openTabs = int.Parse(Console.ReadLine());
+            int salary = int.Parse(Console.ReadLine());
+
+            int fine = 0;
+
+            for (int n = 1; n <= openTabs; n++)
+            {
+                string webName = Console.ReadLine();
+                switch (webName)
+                {
+                    case "Facebook": fine += 150; break;
+                    case "Instagram": fine += 100; break;
+                    case "Reddit": fine += 50; break;
+                }
+                if (fine >= salary)
+                {
+                    Console.WriteLine("You have lost your salary.");
+                    return;
+                }
+            }
+            if (fine < salary)
+            {
+                int restSalary = salary - fine;
+                Console.WriteLine($"{restSalary}");
+            }
+            else if (fine >= salary)
+            {
+                Console.WriteLine("You have lost your salary.");
+            }
+        }
+    }
+}
