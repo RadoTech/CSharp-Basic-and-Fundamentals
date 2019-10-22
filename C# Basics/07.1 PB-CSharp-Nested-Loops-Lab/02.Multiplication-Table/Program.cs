e708ef9283ca0eb4bac1019434ee29f115d67fc0
RADO <53035214+RadoTech@users.noreply.github.com>
2019-10-22 14:09:33 +0300

    Add files via upload

---
+++ C# Basics/07.1 PB-CSharp-Nested-Loops-Lab/02.Multiplication-Table/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int firstNumber = 1; firstNumber <= 10; firstNumber++)
+            {
+                for (int secondNumber = 1; secondNumber <= 10; secondNumber++)
+                {
+                    int result = firstNumber * secondNumber;
+                    Console.WriteLine($"{firstNumber} * {secondNumber} = {result}");
+                }
+            }
+        }
+    }
+}
