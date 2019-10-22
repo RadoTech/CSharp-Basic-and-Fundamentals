cf56b80e52241d7c0c14efd512c4e74c1b2c56cd
RADO <53035214+RadoTech@users.noreply.github.com>
2019-10-22 14:43:39 +0300

    Add files via upload

---
+++ C# Basics/07.1 PB-CSharp-Nested-Loops-Lab/03.Combinations/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Combinations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int combinationsCount = 0;
+
+            for (int firstNumber = 0; firstNumber <= n; firstNumber++)
+            {
+                for (int secondNumber = 0; secondNumber <= n; secondNumber++)
+                {
+                    for (int thirdNumber = 0; thirdNumber <= n; thirdNumber++)
+                    {
+                        int sum = firstNumber + secondNumber + thirdNumber;
+
+                        if (sum == n)
+                        {
+                            combinationsCount++;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine($"{combinationsCount}");
+        }
+    }
+}
