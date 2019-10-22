9b01eb260a4b271a35d10c86e5eb2631147a99f6
RADO <53035214+RadoTech@users.noreply.github.com>
2019-10-22 17:54:40 +0300

    Add files via upload

---
+++ C# Basics/07.1 PB-CSharp-Nested-Loops-Lab/06.Name-Wars/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace NameWars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int charSum = 0;
+            int maxSum = int.MinValue;
+            string maxName = "N/A";
+            
+
+            while (name != "STOP")
+            {
+                int len = name.Length;
+                for (int i = 0; i < len; i++)
+                {
+                    char letter = name[i];
+                    charSum += letter;
+                }
+                if (charSum > maxSum)
+                {
+                    maxSum = charSum;
+                    maxName = name;
+                }
+                charSum = 0;
+                name = Console.ReadLine();
+            }
+            Console.WriteLine($"Winner is {maxName} - {maxSum}!");
+        }
+    }
+}
