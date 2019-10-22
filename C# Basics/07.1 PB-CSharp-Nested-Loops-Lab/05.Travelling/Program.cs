98161c6a4fd11c3858ef3c522796042978476dcc
RADO <53035214+RadoTech@users.noreply.github.com>
2019-10-22 16:58:05 +0300

    Add files via upload

---
+++ C# Basics/07.1 PB-CSharp-Nested-Loops-Lab/05.Travelling/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Travelling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string destination = Console.ReadLine();
+
+            double savedMoney = 0;
+
+            while (destination != "End")
+            {
+                double budgetNeeded = double.Parse(Console.ReadLine());
+
+                for (double saving = 0; savedMoney < budgetNeeded; saving = double.Parse(Console.ReadLine()))
+                {
+                    savedMoney += saving;
+                    if (savedMoney >= budgetNeeded)
+                    {
+                        Console.WriteLine($"Going to {destination}!");
+                        savedMoney = 0;
+                        break;
+                    }
+                }
+                
+                destination = Console.ReadLine();
+
+            }
+        }
+    }
+}
