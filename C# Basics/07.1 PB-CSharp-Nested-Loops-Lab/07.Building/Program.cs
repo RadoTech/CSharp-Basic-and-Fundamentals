8e13f182db1b058194330650f9d90805e10b5e2b
RADO <53035214+RadoTech@users.noreply.github.com>
2019-10-23 11:21:52 +0300

    Add files via upload

---
+++ C# Basics/07.1 PB-CSharp-Nested-Loops-Lab/07.Building/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Building
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int floors = int.Parse(Console.ReadLine());
+            int rooms = int.Parse(Console.ReadLine());
+
+            string roomType = "";
+
+            for (int f = floors; f > 0;f--)
+            {
+                
+                for (int r = 0; r < rooms; r++)
+                {
+                    if (f % 2 == 0)
+                    {
+                        roomType = "O";
+                        if (f == floors)
+                        {
+                            roomType = "L";
+                        }
+                    }
+                    else if (f % 2 != 0)
+                    {
+                        roomType = "A";
+                        if (f == floors)
+                        {
+                            roomType = "L";
+                        }
+                    }
+                    Console.Write($"{roomType}{f}{r} ");
+                }
+                Console.Write("\r\n");
+            }
+        }
+    }
+}
