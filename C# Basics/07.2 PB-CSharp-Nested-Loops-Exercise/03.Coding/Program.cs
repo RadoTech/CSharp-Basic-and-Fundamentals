258a15e000ca49d0bf543989c85074613fb3975d
RADO <53035214+RadoTech@users.noreply.github.com>
2019-10-25 17:16:06 +0300

    Add files via upload

---
+++ C# Basics/07.2 PB-CSharp-Nested-Loops-Exercise/03.Coding/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Coding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input:
+
+            string inputNum = Console.ReadLine();
+
+            for (int i = inputNum.Length - 1; i >= 0; i--)
+            {
+                char currentDigit = inputNum[i];
+                int currentDigitToNum = int.Parse(currentDigit.ToString());
+
+                if (currentDigitToNum == 0)
+                {
+                    Console.WriteLine("ZERO");
+                    continue;
+                }
+
+                for (int n = 1; n <= currentDigitToNum; n++)
+                {
+                    Console.Write((char)(currentDigitToNum + 33));
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
