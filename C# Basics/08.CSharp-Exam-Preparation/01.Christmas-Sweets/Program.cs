e3828521e242083e1859134a168d8b1e2ba5729d
RADO <53035214+RadoTech@users.noreply.github.com>
2019-10-26 17:22:22 +0300

    Add files via upload

---
+++ C# Basics/08.CSharp-Exam-Preparation/01.Christmas-Sweets/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ChristmasSweets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double baklavaPriceKg = double.Parse(Console.ReadLine());
+            double muffinsPriceKg = double.Parse(Console.ReadLine());
+            double stolenKg = double.Parse(Console.ReadLine());
+            double bonboniKg = double.Parse(Console.ReadLine());
+            double biskvitiKg = double.Parse(Console.ReadLine());
+
+            double stolenPriceKg = baklavaPriceKg * 1.6;
+            double bonboniPricekg = muffinsPriceKg * 1.8;
+            double biskvitiPriceKg = 7.50;
+
+            double totalMoneyNeeded = (stolenPriceKg * stolenKg) + (bonboniPricekg * bonboniKg) + biskvitiPriceKg * (biskvitiKg);
+
+            Console.WriteLine($"{totalMoneyNeeded:f2}");
+        }
+    }
+}
