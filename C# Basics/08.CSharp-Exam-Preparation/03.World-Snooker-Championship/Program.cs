f0dfaad12f13cded107c457f25286d4385eb590d
RADO <53035214+RadoTech@users.noreply.github.com>
2019-10-26 17:24:15 +0300

    Add files via upload

---
+++ C# Basics/08.CSharp-Exam-Preparation/03.World-Snooker-Championship/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace WorldSnookerChampionship
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input:
+
+            string championshipStage = Console.ReadLine();
+            string ticketType = Console.ReadLine();
+            int ticketsCount = int.Parse(Console.ReadLine());
+            char photosTrophy = char.Parse(Console.ReadLine());
+
+            //Logic
+
+            double allTicketsSum = 0;
+
+            if (championshipStage == "Quarter final")
+            {
+                if (ticketType == "Standard")
+                {
+                    allTicketsSum = ticketsCount * 55.50;
+                }
+                else if (ticketType == "Premium")
+                {
+                    allTicketsSum = ticketsCount * 105.20;
+                }
+                else if (ticketType == "VIP")
+                {
+                    allTicketsSum = ticketsCount * 118.90;
+                }
+            }
+            else if (championshipStage == "Semi final")
+            {
+                if (ticketType == "Standard")
+                {
+                    allTicketsSum = ticketsCount * 75.88;
+                }
+                else if (ticketType == "Premium")
+                {
+                    allTicketsSum = ticketsCount * 125.22;
+                }
+                else if (ticketType == "VIP")
+                {
+                    allTicketsSum = ticketsCount * 300.40;
+                }
+            }
+            else if (championshipStage == "Final")
+            {
+                if (ticketType == "Standard")
+                {
+                    allTicketsSum = ticketsCount * 110.10;
+                }
+                else if (ticketType == "Premium")
+                {
+                    allTicketsSum = ticketsCount * 160.66;
+                }
+                else if (ticketType == "VIP")
+                {
+                    allTicketsSum = ticketsCount * 400;
+                }
+            }
+
+            double totalSum = 0;
+            int totalPhotosSum = 0;
+
+            if (allTicketsSum > 4000)
+            {
+                totalSum = allTicketsSum * 0.75;
+            }
+            else if (allTicketsSum > 2500)
+            {
+                if (photosTrophy == 'Y')
+                {
+                    totalPhotosSum = ticketsCount * 40;
+                }
+                totalSum = (allTicketsSum * 0.9) + totalPhotosSum;
+            }
+            else if (allTicketsSum <= 2500)
+            {
+                if (photosTrophy == 'Y')
+                {
+                    totalPhotosSum = ticketsCount * 40;
+                }
+                totalSum = allTicketsSum + totalPhotosSum;
+            }
+
+            //Output
+            Console.WriteLine($"{totalSum:f2}");
+        }
+    }
+}
