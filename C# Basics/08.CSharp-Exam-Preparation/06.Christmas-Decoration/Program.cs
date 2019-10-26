e0fda3e45b6c607e6fd5d23e93d3f174181cd93e
RADO <53035214+RadoTech@users.noreply.github.com>
2019-10-26 17:27:18 +0300

    Add files via upload

---
+++ C# Basics/08.CSharp-Exam-Preparation/06.Christmas-Decoration/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ChristmasDecoration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input:
+            int budget = int.Parse(Console.ReadLine());
+            string command = Console.ReadLine();
+
+            //Logic:
+            int currentItemASCISum = 0;
+
+            while (command != "Stop")
+            {
+                for (int i = 0; i < command.Length; i++)
+                {
+                    int currentItemASCI = command[i];
+                    currentItemASCISum += currentItemASCI;
+                }
+                if (currentItemASCISum <= budget)
+                {
+                    Console.WriteLine("Item successfully purchased!");
+                    budget -= currentItemASCISum;
+                }
+                else if (currentItemASCISum > budget)
+                {
+                    Console.WriteLine("Not enough money!");
+                    return;
+                }
+                currentItemASCISum = 0;
+                command = Console.ReadLine();
+            }
+            Console.WriteLine($"Money left: {budget}");
+        }
+    }
+}
