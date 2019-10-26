cc7dcce6ca21bd6b993a3a8a0ee422073e417609
RADO <53035214+RadoTech@users.noreply.github.com>
2019-10-26 17:26:03 +0300

    Add files via upload

---
+++ C# Basics/08.CSharp-Exam-Preparation/05.Movie-Ratings/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace MovieRatings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input:
+
+            int moviesCount = int.Parse(Console.ReadLine());
+
+            //Logic:
+
+            double movieLowestRating = double.MaxValue;
+            double movieHighestRating = double.MinValue;
+            double movieAverageRating = 0;
+
+            string movieLowestRatingName = String.Empty;
+            string movieHighestRatingName = String.Empty;
+
+            
+
+            for (int currentMovie = 1; currentMovie <= moviesCount; currentMovie++)
+            {
+                string currentMovieName = Console.ReadLine();
+                double currentMovieRating = double.Parse(Console.ReadLine());
+
+                if (currentMovieRating > movieHighestRating)
+                {
+                    movieHighestRating = currentMovieRating;
+                    movieHighestRatingName = currentMovieName;
+
+                }
+                if (currentMovieRating < movieLowestRating)
+                {
+                    movieLowestRating = currentMovieRating;
+                    movieLowestRatingName = currentMovieName;
+                }
+                movieAverageRating += currentMovieRating;
+                             
+            }
+            Console.WriteLine($"{movieHighestRatingName} is with highest rating: {movieHighestRating:f1}");
+            Console.WriteLine($"{movieLowestRatingName} is with lowest rating: {movieLowestRating:f1}");
+            Console.WriteLine($"Average rating: {(movieAverageRating / moviesCount):f1}");
+        }
+    }
+}
