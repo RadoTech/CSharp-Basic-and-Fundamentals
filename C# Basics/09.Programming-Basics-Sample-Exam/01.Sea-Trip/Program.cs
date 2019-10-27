fd6a0774bc30d91b3b130ad7ae96107f1e9f132f
RADO <53035214+RadoTech@users.noreply.github.com>
2019-10-27 15:25:02 +0200

    Add files via upload

---
+++ C# Basics/09.Programming-Basics-Sample-Exam/01.Sea-Trip/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SeaTrip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input:
+
+            double foodMoneyADay = double.Parse(Console.ReadLine());
+            double souvenirsMoneyADay = double.Parse(Console.ReadLine());
+            double hotelMoneyADay = double.Parse(Console.ReadLine());
+
+            //Logic
+
+            double fuelMoney = 4.20;
+            double fuelMoneyLiters = (fuelMoney * 7);
+            double fuelMoneyTotal = fuelMoneyLiters * 1.85;
+            double foodMoneyTotal = foodMoneyADay * 3;
+            double souvenirsMoneyTotal = souvenirsMoneyADay * 3;
+            double hotelMoneyTotal = (hotelMoneyADay * 0.9) + (hotelMoneyADay * 0.85) + (hotelMoneyADay * 0.8);
+
+            double moneyTotal = fuelMoneyTotal + foodMoneyTotal + souvenirsMoneyTotal + hotelMoneyTotal;
+
+            //Output:
+
+            Console.WriteLine($"Money needed: {moneyTotal:f2}");
+
+        }
+    }
+}
