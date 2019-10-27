7cd26be42dfae98cf401604fd5c9a4734932009e
RADO <53035214+RadoTech@users.noreply.github.com>
2019-10-27 15:16:29 +0200

    Add files via upload

---
+++ C# Basics/09.Programming-Basics-Sample-Exam/4.Bachelor-Party/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace BachelorParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input
+            int guestMusicianMoney = int.Parse(Console.ReadLine());
+            string command = Console.ReadLine();
+            int peopleOneGroup = 0;
+            int peopleAllGroups = 0;
+            int peopleAllGroupsMoney = 0;
+
+
+            while (command != "The restaurant is full")
+            {
+                peopleOneGroup = int.Parse(command);
+                peopleAllGroups += peopleOneGroup;
+                if (peopleOneGroup >= 5)
+                {
+                    peopleAllGroupsMoney += peopleOneGroup * 70;
+                }
+                else if (peopleOneGroup < 5)
+                {
+                    peopleAllGroupsMoney += peopleOneGroup * 100;
+                }
+                command = Console.ReadLine();
+            }
+            if (peopleAllGroupsMoney >= guestMusicianMoney)
+            {
+                Console.WriteLine($"You have {peopleAllGroups} guests and {peopleAllGroupsMoney - guestMusicianMoney} leva left.");
+            }
+            else if (peopleAllGroupsMoney < guestMusicianMoney)
+            {
+                Console.WriteLine($"You have {peopleAllGroups} guests and {peopleAllGroupsMoney} leva income, but no singer.");
+
+            }
+        }
+    }
+}
