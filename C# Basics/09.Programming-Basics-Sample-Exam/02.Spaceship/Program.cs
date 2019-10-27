91887f17ca9096433d2c467416664cd4c5ecc583
RADO <53035214+RadoTech@users.noreply.github.com>
2019-10-27 15:23:04 +0200

    Add files via upload

---
+++ C# Basics/09.Programming-Basics-Sample-Exam/02.Spaceship/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Spaceship
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input:
+
+            double shipW = double.Parse(Console.ReadLine());
+            double shipL = double.Parse(Console.ReadLine());
+            double shipH = double.Parse(Console.ReadLine());
+            double astronautsAverageH = double.Parse(Console.ReadLine());
+
+            //Logic:
+
+            double shipV = shipW * shipL * shipH;
+            double astronautRoom = 2 * 2 * (astronautsAverageH + 0.4);
+            double astronautsAllRooms = Math.Floor(shipV / astronautRoom);
+
+            //Output:
+
+            if (astronautsAllRooms >= 3 && astronautsAllRooms <= 10)
+            {
+                Console.WriteLine($"The spacecraft holds {astronautsAllRooms} astronauts.");
+            }
+            else if (astronautsAllRooms < 3)
+            {
+                Console.WriteLine("The spacecraft is too small.");
+            }
+            else if (astronautsAllRooms > 10)
+            {
+                Console.WriteLine("The spacecraft is too big.");
+            }
+        }
+    }
+}
