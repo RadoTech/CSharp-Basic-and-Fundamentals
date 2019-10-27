4d9b92fd8e5309e134f9acb1bb3654e55855e6ca
RADO <53035214+RadoTech@users.noreply.github.com>
2019-10-27 15:13:22 +0200

    Add files via upload

---
+++ C# Basics/09.Programming-Basics-Sample-Exam/06.Multiply-Table/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MultiplyTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input:
+            string inputNumber = Console.ReadLine();
+
+            //Logic:
+
+            double firstNumber = 0;
+            double secondNumber = 0;
+            double thirdNumber = 0;
+
+            for (int i = 0; i < inputNumber.Length; i++)
+            {
+                firstNumber = Char.GetNumericValue(inputNumber[0]);
+                secondNumber = Char.GetNumericValue(inputNumber[1]);
+                thirdNumber = Char.GetNumericValue(inputNumber[2]);
+            }
+
+            for (int p = 1; p <= thirdNumber; p++)
+            {
+                for (int j = 1; j <= secondNumber; j++)
+                {
+                    for (int k = 1; k <= firstNumber; k++)
+                    {
+                        Console.WriteLine($"{p} * {j} * {k} = {p * j * k};");
+                    }
+                }
+            }
+        }
+    }
+}
