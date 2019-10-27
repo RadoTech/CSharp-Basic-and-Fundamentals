ee82a39902bc16e1fe49ee99feb7350bc4729e1f
RADO <53035214+RadoTech@users.noreply.github.com>
2019-10-27 15:15:04 +0200

    Add files via upload

---
+++ C# Basics/09.Programming-Basics-Sample-Exam/05.Bus/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Bus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input:
+
+            int passengersCountStart = int.Parse(Console.ReadLine());
+            int stopsCount = int.Parse(Console.ReadLine());
+
+            //Logic:
+            int controllers = 0;
+            int newPassengersIn = 0;
+            int newPassengersOut = 0;
+
+            for (int i = 1; i <= stopsCount; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    controllers = 2;
+                }
+                else if (i % 2 == 0)
+                {
+                    controllers = 0;
+                }
+                newPassengersOut = int.Parse(Console.ReadLine());
+                newPassengersIn = int.Parse(Console.ReadLine());
+                passengersCountStart -= newPassengersOut;
+                passengersCountStart += newPassengersIn;
+            }
+
+            Console.WriteLine($"The final number of passengers is : {passengersCountStart + controllers}");
+        }
+    }
+}
