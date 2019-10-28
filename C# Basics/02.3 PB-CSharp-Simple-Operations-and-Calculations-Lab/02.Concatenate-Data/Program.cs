5b6d6c9836cdbdf37f12d0ae7f34988009905558
RADO <53035214+RadoTech@users.noreply.github.com>
2019-10-28 17:14:02 +0200

    Add files via upload

---
+++ C# Basics/02.3 PB-CSharp-Simple-Operations-and-Calculations-Lab/02.Concatenate-Data/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConcatenateData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = Console.ReadLine();
+            string lastName = Console.ReadLine();
+            int years = int.Parse(Console.ReadLine());
+            string town = Console.ReadLine();
+
+            Console.WriteLine($"You are {firstName} {lastName}, a {years}-years old person from {town}.");
+
+        }
+    }
+}
