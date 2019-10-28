695e8f8c5303583aca1907a3ac1c77cb0dde7698
RADO <53035214+RadoTech@users.noreply.github.com>
2019-10-28 18:14:15 +0200

    Add files via upload

---
+++ C# Basics/01.1 PB-CSharp-First-Steps-in-Coding-Exercises/03.Nums-1-to-10/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Nums1to10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(1);
+            Console.WriteLine(2);
+            Console.WriteLine(3);
+            Console.WriteLine(4);
+            Console.WriteLine(5);
+            Console.WriteLine(6);
+            Console.WriteLine(7);
+            Console.WriteLine(8);
+            Console.WriteLine(9);
+            Console.WriteLine(10);
+
+            //you will learn how to code this better in the "for loop C #" section!
+        }
+    }
+}
