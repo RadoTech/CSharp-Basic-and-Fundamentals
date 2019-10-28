4021e56cf2da0a82f3d0605cdf3e9f7130af92d9
RADO <53035214+RadoTech@users.noreply.github.com>
2019-10-28 17:37:58 +0200

    Add files via upload

---
+++ C# Basics/02.3 PB-CSharp-Simple-Operations-and-Calculations-Lab/07.Pet-Shop/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PetShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //output
+            int dogs = int.Parse(Console.ReadLine());
+            int otherAnimals = int.Parse(Console.ReadLine());
+
+            //logic
+            double dogsMealPrice = 2.5;
+            double otherAnimalsPrice = 4;
+
+            double dogsBill = dogs * dogsMealPrice;
+            double otherAnimalsBill = otherAnimals * otherAnimalsPrice;
+            double totalBill = dogsBill + otherAnimalsBill;
+
+            //output
+
+            Console.WriteLine($"{totalBill:F2} lv.");
+        }
+    }
+}
