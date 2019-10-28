19efc478a01756de7050278dc9b8c698c362c02b
RADO <53035214+RadoTech@users.noreply.github.com>
2019-10-28 17:33:51 +0200

    Add files via upload

---
+++ C# Basics/02.3 PB-CSharp-Simple-Operations-and-Calculations-Lab/06.Circle-Area-and-Perimeter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CircleAreaAndPerimeter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //input
+            double r = double.Parse(Console.ReadLine());
+
+            //logic
+            //Circle Perimeter = S = π.r.r
+            //Circle Area = P = 2.π.r
+
+
+            double S = Math.PI * (r * r);
+            double P = 2 * Math.PI * r;
+
+            //output
+            Console.WriteLine($"{S:F2}");
+            Console.WriteLine($"{P:F2}");
+        }
+    }
+}
