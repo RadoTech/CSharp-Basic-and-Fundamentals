44c1db4b4ec8a60dce2d1c3e2019a3b68b746d2e
RADO <53035214+RadoTech@users.noreply.github.com>
2019-10-28 17:26:12 +0200

    Add files via upload

---
+++ C# Basics/02.3 PB-CSharp-Simple-Operations-and-Calculations-Lab/05.Projects-Creation/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ProjectsCreation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //input
+            string architectNames = Console.ReadLine();
+            int projectsCount = int.Parse(Console.ReadLine());
+
+            //logic
+            int hoursNeeded = projectsCount * 3;
+
+
+            Console.WriteLine($"The architect {architectNames} will need {hoursNeeded} hours to complete {projectsCount} project/s.");
+        }
+    }
+}
