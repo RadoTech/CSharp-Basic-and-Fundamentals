2ba3045c1377f2fedab53e50ca0e545982daf2e8
RADO <53035214+RadoTech@users.noreply.github.com>
2019-10-28 17:19:25 +0200

    Add files via upload

---
+++ C# Basics/02.3 PB-CSharp-Simple-Operations-and-Calculations-Lab/04.Inches-to-Centimeters/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace InchesToCentimeters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double inch = double.Parse(Console.ReadLine());
+            double cmRate = 2.54;
+            double calculation = inch * cmRate;
+
+            Console.WriteLine($"{calculation:F2}");
+        }
+    }
+}
