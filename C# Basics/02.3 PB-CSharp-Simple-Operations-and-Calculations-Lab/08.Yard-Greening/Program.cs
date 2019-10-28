da4dbaf36e24e5f2bf7c0bfe7dd0bf5837c4cc6d
RADO <53035214+RadoTech@users.noreply.github.com>
2019-10-28 17:42:07 +0200

    Add files via upload

---
+++ C# Basics/02.3 PB-CSharp-Simple-Operations-and-Calculations-Lab/08.Yard-Greening/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace YardGreening
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //input
+
+            double sqMeters = double.Parse(Console.ReadLine());
+
+            //logic
+            //1 square meter = 7.61 BGN with taxes
+            //discount 18% of total sum
+
+            double sqMetersRate = 7.61;
+            double sqMeterHome = sqMeters * sqMetersRate;
+            double discount = sqMeterHome * 0.18;
+            double finalPrice = sqMeterHome - discount;
+
+            //output
+
+            Console.WriteLine($"The final price is: {finalPrice:F2} lv.");
+            Console.WriteLine($"The discount is: {discount:F2} lv.");
+
+        }
+    }
+}
