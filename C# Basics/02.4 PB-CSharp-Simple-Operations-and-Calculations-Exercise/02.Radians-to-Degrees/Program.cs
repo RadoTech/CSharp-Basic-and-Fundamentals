4c89c32eb6b73ededf882a3d066c3c4d480ef290
RADO <53035214+RadoTech@users.noreply.github.com>
2019-10-29 12:32:37 +0200

    Add files via upload

---
+++ C# Basics/02.4 PB-CSharp-Simple-Operations-and-Calculations-Exercise/02.Radians-to-Degrees/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercise2RadiansToDegrees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //read corner in radians(rad)
+            double rad = double.Parse(Console.ReadLine());
+
+            //logic transfer rad in degrees (deg) deg = rad * 180/Math.PI
+            double radDegRate = 180 / Math.PI;
+            double deg = rad * radDegRate;
+
+
+            //print out formated with Math.Round()
+            Console.WriteLine(Math.Round(deg));
+
+        }
+    }
+}
