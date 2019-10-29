08cfdb715027d7fa6c9d89ff8915b9dae2c83859
RADO <53035214+RadoTech@users.noreply.github.com>
2019-10-29 12:28:39 +0200

    Add files via upload

---
+++ C# Basics/02.4 PB-CSharp-Simple-Operations-and-Calculations-Exercise/01.USD-to-BGN/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercises1
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            // read input usd
+            double usd = double.Parse(Console.ReadLine());
+
+            //logic(calculations)
+            double usdRate = 1.79549;
+            double bgn = usd * usdRate;
+
+            //output(formated)
+            Console.WriteLine($"{bgn:f2}");
+        }
+    }
+}
