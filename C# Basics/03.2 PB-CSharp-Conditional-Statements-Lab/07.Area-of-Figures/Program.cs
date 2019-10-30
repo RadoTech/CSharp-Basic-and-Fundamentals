f1883cc5e31219a51290a1af143540f96228d1f2
RADO <53035214+RadoTech@users.noreply.github.com>
2019-10-30 14:59:50 +0200

    Add files via upload

---
+++ C# Basics/03.2 PB-CSharp-Conditional-Statements-Lab/07.Area-of-Figures/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace AreaOfFigures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string shape = Console.ReadLine();
+
+            if (shape == "square")
+            {
+                double a = double.Parse(Console.ReadLine());
+                double calculation = a * a;
+                Console.WriteLine($"{calculation:F3}");
+
+            }
+            else if (shape == "rectangle")
+            {
+                double a = double.Parse(Console.ReadLine());
+                double b = double.Parse(Console.ReadLine());
+                double calculation = a * b;
+                Console.WriteLine($"{calculation:F3}");
+            }
+            else if (shape == "circle")
+            {
+                double r = double.Parse(Console.ReadLine());
+                double calculation = Math.PI * Math.Pow(r, 2);
+                Console.WriteLine($"{calculation:F3}");
+            }
+            else if (shape == "triangle")
+            {
+                double a = double.Parse(Console.ReadLine());
+                double b = double.Parse(Console.ReadLine());
+                double calculation = ((a * b) / 2);
+                Console.WriteLine($"{calculation:F3}");
+            }
+        }
+    }
+}
