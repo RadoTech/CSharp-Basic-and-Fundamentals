2f99b33e323c7eec587841d94170c2b71df1f200
RADO <53035214+RadoTech@users.noreply.github.com>
2019-10-30 14:46:46 +0200

    Add files via upload

---
+++ C# Basics/03.2 PB-CSharp-Conditional-Statements-Lab/03.Even-or-Odd/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EvenOrOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            double checkEvenOdd = number % 2;
+
+            if (checkEvenOdd == 0)
+            {
+                Console.WriteLine("even");
+            }
+            else
+            {
+                Console.WriteLine("odd");
+            }
+        }
+    }
+}
