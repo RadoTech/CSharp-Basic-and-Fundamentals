6dc644c5e335f2575323e9c343a9a62f9dd3e915
RADO <53035214+RadoTech@users.noreply.github.com>
2019-11-14 12:03:15 +0200

    Add files via upload

---
+++ C# Basics/Programming-Basics-Online-Exam-2-November-2019-SoftUni/01.Bird-Farm/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BirdFarm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input:
+
+            int oneEggPrice = int.Parse(Console.ReadLine());
+            int hensCountFirstMonth = int.Parse(Console.ReadLine());
+            int hensAddedSecondMonth = int.Parse(Console.ReadLine());
+            int hensAddedThirdMonth = int.Parse(Console.ReadLine());
+
+            //Logic:
+
+            int eggsFirstMonth = hensCountFirstMonth * 20;
+            int eggsSecondMonth = (hensCountFirstMonth + hensAddedSecondMonth) * 20;
+            int eggsThirdMonth = (hensCountFirstMonth + hensAddedSecondMonth + hensAddedThirdMonth) * 20;
+
+            int totalEggs = eggsFirstMonth + eggsSecondMonth + eggsThirdMonth;
+
+            double totalRobustEggs = Math.Floor(totalEggs * 0.96);
+
+            double totalMoneyCoins = totalRobustEggs * oneEggPrice;
+            double totalMoneyBGN = Math.Floor(totalMoneyCoins / 100);
+
+            Console.WriteLine($"Profit: {totalMoneyBGN} Lv.");
+
+        }
+    }
+}
