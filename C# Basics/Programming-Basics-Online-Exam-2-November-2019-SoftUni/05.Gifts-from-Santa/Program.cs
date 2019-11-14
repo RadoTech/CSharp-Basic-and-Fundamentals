c696f03aa9654a90b75cc0590d2d14c3f310e93d
RADO <53035214+RadoTech@users.noreply.github.com>
2019-11-14 11:46:42 +0200

    Add files via upload

---
+++ C# Basics/Programming-Basics-Online-Exam-2-November-2019-SoftUni/05.Gifts-from-Santa/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace GiftsFromSanta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input:
+
+            int N = int.Parse(Console.ReadLine());
+            int M = int.Parse(Console.ReadLine());
+            int S = int.Parse(Console.ReadLine());
+
+            //Logic:
+
+            for (int i = M; i >= N; i--)
+            {
+                if ((i % 2 == 0) && (i % 3 == 0))
+                {
+                    if (i == S)
+                    {
+                        return;
+                    }
+                    else
+                    {
+                        Console.Write($"{i} ");
+                    }
+                }
+            }
+        }
+    }
+}
