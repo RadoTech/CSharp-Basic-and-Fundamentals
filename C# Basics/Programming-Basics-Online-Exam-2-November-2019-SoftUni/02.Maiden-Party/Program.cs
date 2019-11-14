d1146849ae678ae04628f1b70a0d4219818b3386
RADO <53035214+RadoTech@users.noreply.github.com>
2019-11-14 12:00:40 +0200

    Add files via upload

---
+++ C# Basics/Programming-Basics-Online-Exam-2-November-2019-SoftUni/02.Maiden-Party/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace MaidenParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input:
+
+            double maidenPartyPrice = double.Parse(Console.ReadLine());
+            int loveLettersCount = int.Parse(Console.ReadLine());
+            int waxRosesCount = int.Parse(Console.ReadLine());
+            int keyChainsCount = int.Parse(Console.ReadLine());
+            int cartoonsCount = int.Parse(Console.ReadLine());
+            int luckSurprisesCount = int.Parse(Console.ReadLine());
+
+            //Logic:
+
+            double totalItemsSum = ((loveLettersCount * 0.6) + (waxRosesCount * 7.2) + (keyChainsCount * 3.6) + (cartoonsCount * 18.2) + (luckSurprisesCount * 22));
+            int totalItemsCount = loveLettersCount + waxRosesCount + keyChainsCount + cartoonsCount + luckSurprisesCount;
+            double discount = 0;
+
+            if (totalItemsCount >= 25)
+            {
+                discount = totalItemsSum * 0.35;
+                totalItemsSum *= 0.65;
+                
+            }
+
+            double hosting = 0.1;
+            double totalRevenue = totalItemsSum - (totalItemsSum * hosting);
+
+            if (totalRevenue >= maidenPartyPrice)
+            {
+                Console.WriteLine($"Yes! {(totalRevenue - maidenPartyPrice):f2} lv left.");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money! {(maidenPartyPrice - totalRevenue):f2} lv needed.");
+            }
+        }
+    }
+}
