d86f84e6846a6d58b4ba056945b9f10e46f419bb
RADO <53035214+RadoTech@users.noreply.github.com>
2019-11-21 13:49:25 +0200

    Add files via upload

---
+++ C# Basics/03.3 PB-CSharp-Conditional-Statements-Exercise/07.World-Swimming-Record/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace WorldSwimmingRecord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //input
+            //in seconds
+            double worldRecord = double.Parse(Console.ReadLine());
+            //in meters
+            double distance = double.Parse(Console.ReadLine());
+            //in seconds
+            double timeOneMeter = double.Parse(Console.ReadLine());
+
+            //logic
+            double waterResistance = 12.5;
+            double waterResistanceDistance = 15;
+            double realSpeedCorrection = (Math.Floor(distance/waterResistanceDistance)*waterResistance);
+            double ivansTime = (distance * timeOneMeter) + realSpeedCorrection;
+            double timeDifference = worldRecord - ivansTime;
+
+            //output
+
+            if (ivansTime < worldRecord)
+            {
+                Console.WriteLine($"Yes, he succeeded! The new world record is {ivansTime:F2} seconds.");
+            }
+            else if (ivansTime >= worldRecord)
+            {
+                Console.WriteLine($"No, he failed! He was {Math.Abs(timeDifference):F2} seconds slower.");
+            }
+        }
+    }
+}
