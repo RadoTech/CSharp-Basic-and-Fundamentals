d82116aca05196ef5915f5852bf7428bef42d245
RADO <53035214+RadoTech@users.noreply.github.com>
2020-01-17 16:48:06 +0200

    Add files via upload

---
+++ C# Fundamentals/1.BasicSyntax-Conditional-Statements-Loops/1.Ages/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Ages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = int.Parse(Console.ReadLine());
+            string output = String.Empty;
+            if (age >= 0 && age <= 2)
+            {
+                output = "baby";
+            }
+            else if (age > 2 && age <= 13)
+            {
+                output = "child";
+            }
+            else if (age > 13 && age <= 19)
+            {
+                output = "teenager";
+            }
+            else if (age > 19 && age <= 65)
+            {
+                output = "adult";
+            }
+            else if (age > 65)
+            {
+                output = "elder";
+            }
+            Console.WriteLine($"{output}");
+        }
+    }
+}
