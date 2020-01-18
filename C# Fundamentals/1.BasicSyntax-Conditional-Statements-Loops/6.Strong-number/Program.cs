5807c4eb83897dca004bc10a9751655f901b1862
RADO <53035214+RadoTech@users.noreply.github.com>
2020-01-18 16:21:10 +0200

    factorial
    A program to check if a given number is a strong number or not. A number is strong if the sum of the Factorial of each digit is equal to the number. For example 145 is a strong number, because 1! + 4! + 5! = 145. Print "yes" if the number is strong and “no” if the number is not strong.

---
+++ C# Fundamentals/1.BasicSyntax-Conditional-Statements-Loops/6.Strong-number/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace strongNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNum = int.Parse(Console.ReadLine());
+            string inputNumToString = inputNum.ToString();
+            int currentNum = 0;
+            int currentFactorial = 1;
+            int sumFactorial = 0;
+            for (int i = 0; i < inputNumToString.Length ; i++)
+            {
+                currentNum = (int)Char.GetNumericValue(inputNumToString[i]);
+
+                for (int j = 1; j <= currentNum; j++)
+                {
+                    currentFactorial *= j;
+                }
+                sumFactorial += currentFactorial;
+                currentFactorial = 1;
+            }
+            if (sumFactorial == inputNum)
+            {
+                Console.WriteLine("yes");
+            }
+            else if (sumFactorial != inputNum)
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
