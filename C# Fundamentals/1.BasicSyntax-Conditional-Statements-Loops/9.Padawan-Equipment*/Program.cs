5cb80c0b0af699d65638a4c2d9e0ffc06cacb848
RADO <53035214+RadoTech@users.noreply.github.com>
2020-01-18 20:53:48 +0200

    Adding 9. Padawan Equipment*
    Yoda is starting his newly created Jedi academy. So, he asked master Ivan Cho to buy the needed equipment. The number of items depends on how many students will sign up. The equipment for the Padawan contains lightsabers, belts and robes. 
    You will be given the amount of money Ivan Cho has, the number of students and the prices of each item. You have to help Ivan Cho calculate if the money he has is enough to buy all of the equipment, or how much more money he needs. 
    Because the lightsabres sometimes brakes, Ivan Cho should buy 10% more (taken from the students count), rounded up to the next integer. Also, every sixth belt is free. 
    Input / Constraints
    The input data should be read from the console. It will consist of exactly 5 lines:
    •	The amount of money Ivan Cho has – floating-point number in range [0.00…1,000.00]
    •	The count of students – integer in range [0…100]
    •	The price of lightsabers for a single sabre – floating-point number in range [0.00…100.00]
    •	The price of robes for a single robe – floating-point number in range [0.00…100.00]
    •	The price of belts for a single belt – floating-point number in range [0.00…100.00]
    The input data will always be valid. There is no need to check it explicitly.
    Output
    The output should be printed on the console.
    •	If the calculated price of the equipment is less or equal to the money Ivan Cho has:
    o	"The money is enough - it would cost {the cost of the equipment}lv."
    •	If the calculated price of the equipment is more than the money Ivan Cho has:
    o	 "Ivan Cho will need {neededMoney}lv more."
    •	All prices must be rounded to two digits after the decimal point.
    Examples
    Input
    100
    42
    12.0
    4.0
    3.0
    Output
    Ivan Cho will need 737.00lv more.

---
+++ C# Fundamentals/1.BasicSyntax-Conditional-Statements-Loops/9.Padawan-Equipment*/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace padawanEquipment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float money = float.Parse(Console.ReadLine());
+            int padawansCount = int.Parse(Console.ReadLine());
+            float oneLightsabrePrice = float.Parse(Console.ReadLine());
+            float oneRobePrice = float.Parse(Console.ReadLine());
+            float oneBeltPrice = float.Parse(Console.ReadLine());
+            float allLightsabresPrice = (oneLightsabrePrice * padawansCount) + (oneLightsabrePrice * (float)(Math.Ceiling(padawansCount * 0.1)));
+            float allRobesPrice = (oneRobePrice * padawansCount);
+            float discountedBelts = 0;
+            for (int discounted = 0; discounted <= padawansCount; discounted+=6)
+            {
+                if (discounted > 1)
+                {
+                    discountedBelts++;
+                }
+            }
+            float allBeltsPrice = (padawansCount - discountedBelts) * oneBeltPrice;
+            float totalBill = allLightsabresPrice + allRobesPrice + allBeltsPrice;
+            if (totalBill <= money)
+            {
+                Console.WriteLine($"The money is enough - it would cost {totalBill:f2}lv.");
+            }
+            else if (totalBill > money)
+            {
+                Console.WriteLine($"Ivan Cho will need {(totalBill-money):f2}lv more.");
+            }
+        }
+    }
+}
