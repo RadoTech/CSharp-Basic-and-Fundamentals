856ef718741372167eee9dfc9418f4e442414f83
RADO <53035214+RadoTech@users.noreply.github.com>
2020-01-18 19:40:00 +0200

    Adding 07. Vending Machine
    You task is to calculate the total price of a purchase from a vending machine. Until you receive “Start” you will be given different coins that are being inserted in the machine. You have to sum them in order to have the total money inserted. There is a problem though. Your vending machine only works with 0.1, 0.2, 0.5, 1, and 2 coins. If someone tries to insert some other coins you have to display “Cannot accept {money}” and not add it to the total money. On the next few lines until you receive “End” you will be given products to purchase. Your machine has however only “Nuts”, “Water”, “Crisps”, “Soda”, “Coke”. The prices are: 2.0, 0.7, 1.5, 0.8, 1.0 respectively. If the person tries to purchase a not existing product print “Invalid product”. Be careful that the person may try to purchase a product they don’t have the money for. In that case print “Sorry, not enough money”. If the person purchases a product successfully print “Purchased {product name}”. After the “End” command print the money that are left formatted to the second decimal point in the format “Change: {money left}”.

---
+++ C# Fundamentals/1.BasicSyntax-Conditional-Statements-Loops/7.Vending-Machine/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace vendingMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            double insertedMoney = 0;
+            while (command != "Start")
+            {
+                double currentCoin;
+                Double.TryParse(command, out currentCoin);
+                switch (currentCoin)
+                {
+                    case 0.1:
+                    case 0.2:
+                    case 0.5:
+                    case 1:
+                    case 2:
+                        insertedMoney += currentCoin;
+                        break;
+                    default:
+                        Console.WriteLine($"Cannot accept {currentCoin}");
+                        break;
+                }
+                command = Console.ReadLine();
+            }
+            string item = Console.ReadLine();
+            while (item != "End")
+            {
+                if (item == "Nuts")
+                {
+                    if (insertedMoney >= 2.0)
+                    {
+                        insertedMoney -= 2.0;
+                        Console.WriteLine($"Purchased {item.ToLower()}");
+                    }
+                    else if (insertedMoney < 2.0)
+                    {
+                        Console.WriteLine("Sorry, not enough money");
+                    }
+
+                }
+                else if (item == "Water")
+                {
+                    if (insertedMoney >= 0.7)
+                    {
+                        insertedMoney -= 0.7;
+                        Console.WriteLine($"Purchased {item.ToLower()}");
+                    }
+                    else if (insertedMoney < 0.7)
+                    {
+                        Console.WriteLine("Sorry, not enough money");
+                    }
+                }
+                else if (item == "Crisps")
+                {
+                    if (insertedMoney >= 1.5)
+                    {
+                        insertedMoney -= 1.5;
+                        Console.WriteLine($"Purchased {item.ToLower()}");
+                    }
+                    else if (insertedMoney < 1.5)
+                    {
+                        Console.WriteLine("Sorry, not enough money");
+                    }
+                }
+                else if (item == "Soda")
+                {
+                    if (insertedMoney >= 0.8)
+                    {
+                        insertedMoney -= 0.8;
+                        Console.WriteLine($"Purchased {item.ToLower()}");
+                    }
+                    else if (insertedMoney < 0.8)
+                    {
+                        Console.WriteLine("Sorry, not enough money");
+                    }
+                }
+                else if (item == "Coke")
+                {
+                    if (insertedMoney >= 1.0)
+                    {
+                        insertedMoney -= 1.0;
+                        Console.WriteLine($"Purchased {item.ToLower()}");
+                    }
+                    else if (insertedMoney < 1.0)
+                    {
+                        Console.WriteLine("Sorry, not enough money");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid product");
+                }
+                item = Console.ReadLine();
+            }
+            Console.WriteLine($"Change: {insertedMoney:f2}");
+        }
+    }
+}
