8c52595394f5fda0724d8eaeb43bc6ee96340768
RADO <53035214+RadoTech@users.noreply.github.com>
2020-01-18 21:40:06 +0200

    Adding 10. Rage Expenses*
    As a MOBA challenger player, Pesho has the bad habit to trash his PC when he loses a game and rage quits. His gaming setup consists of headset, mouse, keyboard and display. You will receive Pesho`s lost games count. 
    Every second lost game, Pesho trashes his headset.
    Every third lost game, Pesho trashes his mouse.
    When Pesho trashes both his mouse and headset in the same lost game, he also trashes his keyboard.
    Every second time, when he trashes his keyboard, he also trashes his display. 
    You will receive the price of each item in his gaming setup. Calculate his rage expenses for renewing his gaming equipment. 
    Input / Constraints
    •	On the first input line - lost games count – integer in the range [0, 1000].
    •	On the second line – headset price - floating point number in range [0, 1000]. 
    •	On the third line – mouse price - floating point number in range [0, 1000]. 
    •	On the fourth line – keyboard price - floating point number in range [0, 1000]. 
    •	On the fifth line – display price - floating point number in range [0, 1000]. 
    Output
    •	As output you must print Pesho`s total expenses: "Rage expenses: {expenses} lv."
    •	Allowed working time / memory: 100ms / 16MB.
    Examples
    Input
    23
    12.50
    21.50
    40
    200
    Output
    Rage expenses: 608.00 lv.

---
+++ C# Fundamentals/1.BasicSyntax-Conditional-Statements-Loops/10.Rage-Expenses*/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace rageExpenses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lostGamesCount = int.Parse(Console.ReadLine());
+            float headsetPrice = float.Parse(Console.ReadLine());
+            float mousePrice = float.Parse(Console.ReadLine());
+            float keyboardPrice = float.Parse(Console.ReadLine());
+            float displayPrice = float.Parse(Console.ReadLine());
+            int trashedHeadset = 0;
+            int trashedMouse = 0;
+            int trashedKeyboard = 0;
+            int trashedKeyMarker = 0;
+            int trashedDisplay = 0;
+            for (int lostGame = 1; lostGame <= lostGamesCount; lostGame++)
+            {
+                if (lostGame % 2 == 0)
+                {
+                    trashedHeadset++;
+                }
+                if (lostGame % 3 == 0)
+                {
+                    trashedMouse++;
+                }
+                if (lostGame % 2 == 0 && lostGame % 3 == 0)
+                {
+                    trashedKeyboard++;
+                    trashedKeyMarker++;
+                }
+                if (trashedKeyMarker == 2)
+                {
+                    trashedDisplay++;
+                    trashedKeyMarker -= 2;
+                }
+            }
+            float rageExpenses = (trashedHeadset * headsetPrice) + (trashedMouse * mousePrice) + (trashedKeyboard * keyboardPrice) + (trashedDisplay * displayPrice);
+            Console.WriteLine($"Rage expenses: {rageExpenses:f2} lv.");
+        }
+    }
+}
