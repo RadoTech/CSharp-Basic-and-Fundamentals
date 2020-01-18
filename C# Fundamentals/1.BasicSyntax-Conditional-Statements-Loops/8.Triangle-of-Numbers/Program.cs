6e478a2c6642635a3eb9d5e34c41834b81d07402
RADO <53035214+RadoTech@users.noreply.github.com>
2020-01-18 20:13:03 +0200

    Adding 8. Triangle of Numbers
    Write a program, which receives a number – n, and prints a triangle from 1 to n as in the examples.
    •	n will be in the interval [1...20].
    Input	
    3
    Output
    1
    2 2 
    3 3 3

---
+++ C# Fundamentals/1.BasicSyntax-Conditional-Statements-Loops/8.Triangle-of-Numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace triangleOfNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string print = String.Empty;
+            for (int rows = 1; rows <= n; rows++)
+            {
+                for (int collumns = 1; collumns <= rows; collumns++)
+                {
+                    print += (rows + " ");
+                }
+                Console.Write(print);   
+                Console.WriteLine();
+                print = String.Empty;
+            }
+        }
+    }
+}
