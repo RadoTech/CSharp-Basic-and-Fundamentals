0081123cc7f1646f12df0962376ea360092f9cb6
RADO <53035214+RadoTech@users.noreply.github.com>
2020-01-27 13:39:56 +0200

    11. Snowballs*** //Using BigInteger
    Tony and Andi love playing in the snow and having snowball fights, but they always argue which makes the best snowballs. They have decided to involve you in their fray, by making you write a program, which calculates snowball data, and outputs the best snowball value.
    You will receive N – an integer, the number of snowballs being made by Tony and Andi.
    For each snowball you will receive 3 input lines:
    •	On the first line you will get the snowballSnow – an integer.
    •	On the second line you will get the snowballTime – an integer.
    •	On the third line you will get the snowballQuality – an integer.
    For each snowball you must calculate its snowballValue by the following formula:
    (snowballSnow / snowballTime) ^ snowballQuality
    At the end you must print the highest calculated snowballValue.
    Input
    •	On the first input line you will receive N – the number of snowballs.
    •	On the next N * 3 input lines you will be receiving data about snowballs. 
    Output
    •	As output you must print the highest calculated snowballValue, by the formula, specified above. 
    •	The output format is: 
    {snowballSnow} : {snowballTime} = {snowballValue} ({snowballQuality})
    Constraints
    •	The number of snowballs (N) will be an integer in range [0, 100].
    •	The snowballSnow is an integer in range [0, 1000].
    •	The snowballTime is an integer in range [1, 500].
    •	The snowballQuality is an integer in range [0, 100].
    •	Allowed working time / memory: 100ms / 16MB.
    Examples:
    Input:
    2
    10
    2
    3
    5
    5
    5
    --
    3
    10
    5
    7
    16
    4
    2
    20
    2
    2
    Output:
    10 : 2 = 125 (3)
    --
    10 : 5 = 128 (7)

---
+++ C# Fundamentals/2.Data-Types-and-Variables/11.Snowballs*/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Numerics;
+
+namespace Snowballs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int snowballsCount = int.Parse(Console.ReadLine());
+            int snowballSnow = int.Parse(Console.ReadLine());
+            int snowballTime = int.Parse(Console.ReadLine());
+            int snowballQuality = int.Parse(Console.ReadLine());
+            BigInteger snowballValue = 0;
+            BigInteger snowballMaxValue = -1;
+            int snowballMaxSnow = 0;
+            int snowballMaxTime = 0;
+            int snowballMaxQuality = 0;
+            for (int snowball = 1; snowball <= snowballsCount; snowball++)
+            {
+                int snowTimeToDouble = (snowballSnow / snowballTime);
+                snowballValue = BigInteger.Pow(snowTimeToDouble, snowballQuality);
+                if (snowballValue > snowballMaxValue)
+                {
+                    snowballMaxValue = snowballValue;
+                    snowballMaxSnow = snowballSnow;
+                    snowballMaxTime = snowballTime;
+                    snowballMaxQuality = snowballQuality;
+                }
+                if (snowball == snowballsCount)
+                {
+                    break;
+                }
+                snowballSnow = int.Parse(Console.ReadLine());
+                snowballTime = int.Parse(Console.ReadLine());
+                snowballQuality = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine($"{snowballMaxSnow} : {snowballMaxTime} = {snowballMaxValue} ({snowballMaxQuality})");
+        }
+    }
+}
