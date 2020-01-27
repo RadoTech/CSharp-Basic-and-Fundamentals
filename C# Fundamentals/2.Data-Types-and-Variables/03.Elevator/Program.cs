7b60c2a0339cdbce5670e3161d24bc9e7b8c49df
RADO <53035214+RadoTech@users.noreply.github.com>
2020-01-27 13:14:52 +0200

    3.	Elevator
    Calculate how many courses will be needed to elevate n persons by using an elevator of capacity of p persons. The input holds two lines: the number of people n and the capacity p of the elevator.
    Examples
    Input
    17
    3
    --
    4
    5
    Output
    6
    --
    1

---
+++ C# Fundamentals/2.Data-Types-and-Variables/03.Elevator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int people = int.Parse(Console.ReadLine());
+            int capacity = int.Parse(Console.ReadLine());
+            if (people % capacity == 0)
+            {
+                Console.WriteLine(people / capacity);
+            }
+            else if (people < capacity)
+            {
+                Console.WriteLine(1);
+            }
+            else if (people % capacity != 0)
+            {
+                int result = (people / capacity) + 1;
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
