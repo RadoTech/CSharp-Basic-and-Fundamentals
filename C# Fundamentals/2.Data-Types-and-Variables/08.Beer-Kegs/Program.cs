dda0d4a319df65737b6b99dbb7e2e0b57a7b5feb
RADO <53035214+RadoTech@users.noreply.github.com>
2020-01-27 13:29:37 +0200

    8. Beer Kegs
    Write a program, which calculates the volume of n beer kegs. You will receive in total 3 * n lines. Each three lines will hold information for a single keg. First up is the model of the keg, after that is the radius of the keg, and lastly is the height of the keg.
    Calculate the volume using the following formula: π * r^2 * h. 
    At the end, print the model of the biggest keg.
    Input
    You will receive 3 * n lines. Each group of lines will be on a new line:
    •	First – model – string.
    •	Second –radius – floating-point number
    •	Third – height – integer number
    Output
    Print the model of the biggest keg.
    Constraints
    •	n will be in the interval [1…10]
    •	The radius will be a floating-point number in the interval [1…3.402823E+38]
    •	The height will be an integer in the interval [1…2147483647]
    Examples:
    Input:
    3
    Keg 1
    10
    10
    Keg 2
    20
    20
    Keg 3
    10
    30
    --
    2
    Smaller Keg
    2.41
    10
    Bigger Keg
    5.12
    20
    --
    Output:
    Keg 2
    --
    Bigger Keg

---
+++ C# Fundamentals/2.Data-Types-and-Variables/08.Beer-Kegs/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace beerKegs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int kegCount = int.Parse(Console.ReadLine());
+            string kegName = Console.ReadLine();
+            double kegRadius = double.Parse(Console.ReadLine());
+            int kegHeight = int.Parse(Console.ReadLine());
+            decimal biggestKeg = 0.0M;
+            string biggestKegName = string.Empty;
+            for (int keg = 1; keg <= kegCount; keg++)
+            {
+                decimal kegVolume = (decimal)Math.PI * (decimal)Math.Pow(kegRadius, 2) * kegHeight;
+                if (kegVolume > biggestKeg)
+                {
+                    biggestKeg = kegVolume;
+                    biggestKegName = kegName; 
+                }
+                if (keg == kegCount)
+                {
+                    break;
+                }
+                kegName = Console.ReadLine();
+                kegRadius = double.Parse(Console.ReadLine());
+                kegHeight = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(biggestKegName);
+        }
+    }
+}
