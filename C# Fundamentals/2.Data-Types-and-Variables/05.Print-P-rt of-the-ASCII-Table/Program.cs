fdf510c809b8db3111affe5a4eeba845b2b8954b
RADO <53035214+RadoTech@users.noreply.github.com>
2020-01-27 13:19:16 +0200

    5. Print Part of the ASCII Table
    Find online more information about ASCII (American Standard Code for Information Interchange) and write a program that prints part of the ASCII table of characters at the console.  On the first line of input you will receive the char index you should start with and on the second line - the index of the last character you should print.
    Examples:
    Input:
    60
    65
    --
    69
    79
    --
    97
    104
    --
    40
    55
    --
    Output:
    < = > ? @ A
    E F G H I J K L M N O
    a b c d e f g h
    ( ) * + , - . / 0 1 2 3 4 5 6 7

---
+++ C# Fundamentals/2.Data-Types-and-Variables/05.Print-P-rt of-the-ASCII-Table/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace printPartOfASCIITable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startChar = int.Parse(Console.ReadLine());
+            int endChar = int.Parse(Console.ReadLine());
+            for (int i = startChar; i <= endChar; i++)
+            {
+                char currentChar = (char)i;
+                Console.Write($"{currentChar} ");
+            }
+        }
+    }
+}
