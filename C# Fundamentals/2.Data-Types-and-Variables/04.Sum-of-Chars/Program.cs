d4d3757c25cd9d5dd3c9576acee29c9f8d55c4d2
RADO <53035214+RadoTech@users.noreply.github.com>
2020-01-27 13:16:54 +0200

    4. Sum of Chars
    Write a program, which sums the ASCII codes of n characters and prints the sum on the console.
    Input
    •	On the first line, you will receive n – the number of lines, which will follow
    •	On the next n lines – you will receive letters from the Latin alphabet
    Output
    Print the total sum in the following format:
    The sum equals: {totalSum}
    Constraints
    •	n will be in the interval [1…20].
    •	The characters will always be either upper or lower-case letters from the English alphabet
    •	You will always receive one letter per line
    Examples:
    Input:
    5
    A
    b
    C
    d
    E
    --
    12
    S
    o
    f
    t
    U
    n
    i
    R
    u
    l
    z
    z
    Output:
    The sum equals: 399
    --
    The sum equals: 1263

---
+++ C# Fundamentals/2.Data-Types-and-Variables/04.Sum-of-Chars/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace sumOfChars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            int sumDigits = 0;
+            if (lines == 0)
+            {
+                Console.WriteLine($"The sum equals: {sumDigits}");
+            }
+            else if (lines > 0)
+            {
+                char letter = char.Parse(Console.ReadLine());
+                for (int i = 1; i <= lines; i++)
+                {
+                    int digit = letter;
+                    sumDigits += digit;
+                    if (i == lines)
+                    {
+                        break;
+                    }
+                    letter = char.Parse(Console.ReadLine());
+                }
+                Console.WriteLine($"The sum equals: {sumDigits}");
+            }
+        }
+    }
+}
