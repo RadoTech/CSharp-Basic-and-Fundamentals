037066d41118c2cff85b38bbc46e293d8526229b
RADO <53035214+RadoTech@users.noreply.github.com>
2020-01-27 13:13:19 +0200

    Add files via upload

---
+++ C# Fundamentals/2.Data-Types-and-Variables/02.Sum-Digits/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SumDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+            int sumDigits = 0;
+            for (int i = 0; i < number.Length; i++)
+            {
+                int digit = (int)Char.GetNumericValue(number[i]);
+                sumDigits += digit;
+            }
+            Console.WriteLine(sumDigits);
+        }
+    }
+}
