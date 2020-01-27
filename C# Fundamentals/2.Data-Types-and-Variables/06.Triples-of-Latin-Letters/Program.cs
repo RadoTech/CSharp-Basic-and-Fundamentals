f6b3e3fc63bcc22985572f5a11b5dbf28296e87f
RADO <53035214+RadoTech@users.noreply.github.com>
2020-01-27 13:24:11 +0200

    6. Triples of Latin Letters
    Write a program to read an integer n and print all triples of the first n small Latin letters, ordered alphabetically:
    Examples:
    Input:
    3
    Output:
    aaa
    aab
    aac
    aba
    abb
    abc
    aca
    acb
    acc
    baa
    bab
    bac
    bba
    bbb
    bbc
    bca
    bcb
    bcc
    caa
    cab
    cac
    cba
    cbb
    cbc
    cca
    ccb
    ccc

---
+++ C# Fundamentals/2.Data-Types-and-Variables/06.Triples-of-Latin-Letters/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace triplesOfLatinLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNumber = int.Parse(Console.ReadLine());
+            for (int first = 1; first <= inputNumber; first++)
+            {
+                for (int second = 1; second <= inputNumber; second++)
+                {
+                    for (int third = 1; third <= inputNumber; third++)
+                    {
+                        char firstChar = (char)(first + 96);
+                        char secondChar = (char)(second + 96);
+                        char thirdChar = (char)(third + 96);
+                        Console.WriteLine($"{firstChar}{secondChar}{thirdChar}");
+                    }
+                }
+            }
+        }
+    }
+}
