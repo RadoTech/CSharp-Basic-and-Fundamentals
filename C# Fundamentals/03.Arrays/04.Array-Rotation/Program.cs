a032d59ef4590c4832ff75768b65f61a8e83631a
RADO <53035214+RadoTech@users.noreply.github.com>
2020-02-01 17:43:04 +0200

    Adding 04. Array Rotation (C# Fundamentals/Arrays)

---
+++ C# Fundamentals/03.Arrays/04.Array-Rotation/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+
+namespace arrayRotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int n = int.Parse(Console.ReadLine());
+            for (int rotate = 1; rotate <= n; rotate++)
+            {
+                var temp = inputArray[0];
+                for (var i = 1; i < inputArray.Length; i++)
+                {
+                    inputArray[i - 1] = inputArray[i];
+                }
+                inputArray[inputArray.Length - 1] = temp;
+            }        
+            Console.WriteLine(String.Join(" ", inputArray));
+        }
+    }
+}
