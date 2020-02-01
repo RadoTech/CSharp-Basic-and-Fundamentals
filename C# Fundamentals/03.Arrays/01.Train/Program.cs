6990fbf18fe8f3175ff80a459e14a840b13dc9c2
RADO <53035214+RadoTech@users.noreply.github.com>
2020-02-01 16:02:46 +0200

    Adding 1. Train
    You will be given a count of wagons in a train n. On the next n lines you will receive how many people are going to get on each wagon. At the end print the whole train and after that, on the next line, the sum of the people in the train. 
    Examples:
    Input
    3
    13
    24
    8
    Output:
    13 24 8
    45
    Input:
    6
    3
    52
    71
    13
    65
    4
    Output:
    3 52 71 13 65 4
    208
    Input:
    1
    100
    Output:
    100
    100

---
+++ C# Fundamentals/03.Arrays/01.Train/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int wagonsCount = int.Parse(Console.ReadLine());
+            int[] wagons = new int[wagonsCount];
+            int passengersSum = 0;
+            string passengersRow = string.Empty;
+            foreach (var i in wagons)
+            {
+                int passengers = int.Parse(Console.ReadLine());
+                passengersRow += passengers + " ";
+                passengersSum += passengers;
+            }
+            Console.WriteLine(passengersRow);
+            Console.WriteLine(passengersSum);
+        }
+    }
+}
