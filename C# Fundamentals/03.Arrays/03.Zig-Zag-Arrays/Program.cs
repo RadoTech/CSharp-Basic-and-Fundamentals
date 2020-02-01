58f191cbc57641cabef954aec99d6df7e02bcbae
RADO <53035214+RadoTech@users.noreply.github.com>
2020-02-01 15:56:17 +0200

    Adding 3. Zig-Zag Arrays
    Write a program which creates 2 arrays. You will be given an integer n. On the next n lines you get 2 integers. Form 2 arrays as shown below.
    Examples:
    Input:
    4
    1 5
    9 10
    31 81
    41 20
    Output:
    1 10 31 20
    5 9 81 41
    Input:
    2
    80 23
    31 19
    Output:
    80 19
    23 31

---
+++ C# Fundamentals/03.Arrays/03.Zig-Zag-Arrays/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace zigZagArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] joinArray = new int[n];
+            int[] firstArray = new int[n];
+            int[] secondArray = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                joinArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                if (i % 2 == 0)
+                {
+                    firstArray[i] = joinArray[0];
+                    secondArray[i] = joinArray[1];
+                }
+                else if (i % 2 != 0)
+                {
+                    firstArray[i] = joinArray[1];
+                    secondArray[i] = joinArray[0];
+                }
+            }
+            Console.WriteLine(String.Join(" ", firstArray));
+            Console.WriteLine(String.Join(" ", secondArray));
+        }
+    }
+}
