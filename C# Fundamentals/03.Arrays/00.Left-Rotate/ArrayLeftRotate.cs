9d4cab162dda3d8e5c469a3af5a85fcd87e75d15
RADO <53035214+RadoTech@users.noreply.github.com>
2020-02-01 17:15:47 +0200

    Left Rotate Array

---
+++ C# Fundamentals/03.Arrays/00.Left-Rotate/ArrayLeftRotate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercisesProject.Exercises.Geometry
+{
+    class ArrayLeftRotate
+    {
+        public static void Run()
+        {
+            var array = new string[] { "a", "b", "c" };
+
+            Console.WriteLine($"Before rotate {string.Join(", ", array)}");
+            LeftRotate(array);
+            Console.WriteLine($"After rotate {string.Join(", ", array)}");
+
+            Console.ReadKey();
+        }
+
+        private static void LeftRotate( string[] array )
+        {
+            var first = array[0];
+
+            for (var i = 1; i < array.Length; i++)
+            {
+                array[i - 1] = array[i];
+            }
+
+            array[array.Length - 1] = first;
+        }
+    }
+}
