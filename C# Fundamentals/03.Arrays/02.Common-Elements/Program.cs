54cbba8cb8d0fb3a341d0bb9b4db338f8c15554d
RADO <53035214+RadoTech@users.noreply.github.com>
2020-02-01 15:59:42 +0200

    Adding 2. Common Elements
    Write a program, which prints common elements in two arrays. You have to compare the elements of the second array to the elements of the first.
    Examples:
    Input:
    Hey hello 2 4
    10 hey 4 hello
    Output:
    4 hello
    Input:
    S of t un i
    of i 10 un
    Output:
    of i un
    Input:
    i love to code
    code i love to
    Output:
    code i love to

---
+++ C# Fundamentals/03.Arrays/02.Common-Elements/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace commonElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arrayOne = Console.ReadLine().Split();
+            string[] arrayTwo = Console.ReadLine().Split();
+            for (int second = 0; second < arrayTwo.Length; second++)
+            {
+                for (int first = 0; first < arrayOne.Length; first++)
+                {
+                    if (arrayTwo[second] == arrayOne[first])
+                    {
+                        string match = arrayOne[first];
+                        Console.Write(String.Join(" ",match + " "));
+                    }
+                }
+            }
+        }
+    }
+}
