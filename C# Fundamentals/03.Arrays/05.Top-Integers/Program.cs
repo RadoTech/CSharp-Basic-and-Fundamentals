7718b90b06af59fb5c8b94e6f81a1896063c248f
RADO <53035214+RadoTech@users.noreply.github.com>
2020-02-03 22:56:42 +0200

    05. Top Integers* (comparison of array elements[])
    5.	Top Integers
    Write a program to find all the top integers in an array. A top integer is an integer which is bigger than all the elements to its right. 
    Examples
    Input
    1 4 3 2
    14 24 3 19 15 17
    27 19 42 2 13 45 48
    Output
    4 3 2
    24 19 17
    48

---
+++ C# Fundamentals/03.Arrays/05.Top-Integers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace topIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            string result = String.Empty;
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                int current = inputArray[i];
+                bool isTopInteger = true;
+                for (int j = i+1; j < inputArray.Length; j++)
+                {
+                    int compare = inputArray[j];
+                    if (current <= compare)
+                    {
+                        isTopInteger = false;
+                        break;
+                    }
+                }
+                if (isTopInteger == true)
+                {
+                    result += (current + " ");
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
