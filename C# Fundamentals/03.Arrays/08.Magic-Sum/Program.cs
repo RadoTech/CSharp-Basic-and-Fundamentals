34139aad3635ad760bd6ac9967889c631b576fd4
RADO <53035214+RadoTech@users.noreply.github.com>
2020-02-04 13:03:07 +0200

    8. Magic Sum (Array Exercises)
    8. Magic Sum
    Write a program, which prints all unique pairs in an array of integers whose sum is equal to a given number. 
    Examples:
    Inputs:
    
    1 7 6 2 19 23
    8
    
    14 20 60 13 7 19 8 
    27
    
    Outputs:
    
    1 7
    6 2
    
    14 13
    20 7
    19 8

---
+++ C# Fundamentals/03.Arrays/08.Magic-Sum/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace magicSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int magicNum = int.Parse(Console.ReadLine());
+            for (int first = 0; first < array.Length; first++)
+            {
+                int firstNum = array[first];
+                for (int next = first + 1; next < array.Length; next++)
+                {
+                    int nextNum = array[next];
+                    int sumNum = firstNum + nextNum;
+                    if (sumNum == magicNum)
+                    {
+                        int[] magicArray = new int[2];
+                        magicArray[0] = firstNum;
+                        magicArray[1] = nextNum;
+                        Console.WriteLine(String.Join(" ", magicArray));
+                    }
+                }
+            }
+        }
+    }
+}
