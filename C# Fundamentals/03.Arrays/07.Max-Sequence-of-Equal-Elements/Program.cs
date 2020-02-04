b2627d1cec6117b06621d373d891f372d9ebb5c1
RADO <53035214+RadoTech@users.noreply.github.com>
2020-02-04 12:37:03 +0200

    7. Max Sequence of Equal Elements - Array Exercise
    7.	Max Sequence of Equal Elements
    Write a program that finds the longest sequence of equal elements in an array of integers. If several longest sequences exist, print the leftmost one.
    Examples:
    Input:
    2 1 1 2 3 3 2 2 2 1
    1 1 1 2 3 1 3 3
    4 4 4 4
    0 1 1 5 2 2 6 3 3
    
    Output:
    2 2 2
    1 1 1
    4 4 4 4
    1 1

---
+++ C# Fundamentals/03.Arrays/07.Max-Sequence-of-Equal-Elements/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace maxSequenceOfEqualElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            string match = string.Empty;
+            string bestMatch = string.Empty;
+            string firstMatch = string.Empty;
+            for (int first = 0; first < array.Length; first++)
+            {
+                int currentFirst = array[first];
+                for (int second = first + 1; second < array.Length; second++)
+                {
+                    int currentSecond = array[second];
+                    if (currentFirst == currentSecond)
+                    {
+                        match += currentFirst + " ";
+                    }
+                    else if (first != second)
+                    {
+                        break;
+                    }
+                }
+                firstMatch = match + currentFirst;
+                match = string.Empty;
+                if (firstMatch.Length > bestMatch.Length)
+                {
+                    bestMatch = firstMatch;
+                }
+            }
+            Console.WriteLine(bestMatch);
+        }
+    }
+}
