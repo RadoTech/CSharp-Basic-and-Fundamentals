9f634aacf76133ef6d84269d3e5d036a87bb6c12
RADO <53035214+RadoTech@users.noreply.github.com>
2020-02-06 14:38:13 +0200

    Kamino Factory*** Applying while loop after 1st it
    9. Kamino Factory*** - Applying while loop after making sure that the logic for the first iteration is already in place
    
    The clone factory in Kamino got another order to clone troops. But this time you are tasked to find the best DNA sequence to use in the production. 
    You will receive the DNA length and until you receive the command "Clone them!" you will be receiving a DNA sequences of ones and zeroes, split by "!" (one or several).
    You should select the sequence with the longest subsequence of ones. If there are several sequences with same length of subsequence of ones, print the one with the leftmost starting index, if there are several sequences with same length and starting index, select the sequence with the greater sum of its elements.
    After you receive the last command "Clone them!" you should print the collected information in the following format:
    "Best DNA sample {bestSequenceIndex} with sum: {bestSequenceSum}."
    "{DNA sequence, joined by space}"
    Input / Constraints
    •	The first line holds the length of the sequences – integer in range [1…100];
    •	On the next lines until you receive "Clone them!" you will be receiving sequences (at least one) of ones and zeroes, split by "!" (one or several).
     Output
    The output should be printed on the console and consists of two lines in the following format:
    "Best DNA sample {bestSequenceIndex} with sum: {bestSequenceSum}."
    "{DNA sequence, joined by space}"
     Examples:
    Input:
    
    5
    1!0!1!1!0
    0!1!1!0!0
    Clone them!
    
    Output:
    
    Best DNA sample 2 with sum: 2.
    0 1 1 0 0
    
    Input:
    
    4
    1!1!0!1
    1!0!0!1
    1!1!0!0
    Clone them!
    
    Output:
    
    Best DNA sample 1 with sum: 3.
    1 1 0 1

---
+++ C# Fundamentals/03.Arrays/09.Kamino-Factory***/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq;
+
+namespace KaminoFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] maxArray = new int[n];
+            int maxCount = 0;
+            int maxIndex = 0;
+            int maxSample = 1; //every input will has at least 1st sample, hence maxSample starts at 1;
+            int currSample = 0;
+
+            while (true) //!!!while loop declaring before the input "line"!!! and put all logic for one turn into the while loop!!!
+            {
+                string line = Console.ReadLine();
+                if (line == "Clone them!")
+                {
+                    break;
+                }
+                int[] currArr = line //changed to "line" from "Console.ReadLine()", after declaring the while loop
+                               .Split("!", StringSplitOptions.RemoveEmptyEntries)
+                               .Select(int.Parse)
+                               .ToArray();
+                currSample++;
+                int bestCurrCount = 0; //ctrl+RR from "bestCount" to "bestCurrCount" after declaring the while loop
+                int bestCurrIndex = 0; //ctrl+RR from "bestIndex" to "bestCurrIndex" after declaring the while loop
+                int bestCurrSum = 0;   //ctrl+RR from "bestSum" to "bestCurrSum" after declaring the while loop
+
+
+                for (int currIndex = 0; currIndex < currArr.Length; currIndex++)
+                {
+                    int currElement = currArr[currIndex];
+                    int currCount = 1;
+                    if (currElement == 0)
+                    {
+                        continue;
+                    }
+                    for (int index = currIndex + 1; index < currArr.Length; index++)
+                    {
+                        if (currArr[index] == 1)
+                        {
+                            currCount++;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                    if (currCount > bestCurrCount)
+                    {
+                        bestCurrCount = currCount;
+                        bestCurrIndex = currIndex;
+                        bestCurrSum = currArr.Sum();
+                        
+                    }
+                }
+                //after the array(when the best has been found)
+                if ((bestCurrCount > maxCount) ||
+                   (bestCurrCount == maxCount && maxIndex > bestCurrIndex) ||
+                   (maxArray.Sum() < bestCurrSum))                 
+                {
+                    maxIndex = bestCurrIndex;
+                    maxCount = bestCurrCount;
+                    maxArray = currArr;
+                    maxSample = currSample;
+                }
+            }
+            Console.WriteLine($"Best DNA sample {maxSample} with sum: {maxArray.Sum()}.");
+            Console.WriteLine(string.Join(" ", maxArray));
+        }
+    }
+}
