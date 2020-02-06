0a69e13fac73c0986b9667f1f71f36725df8a6ab
RADO <53035214+RadoTech@users.noreply.github.com>
2020-02-06 13:02:40 +0200

    Add files via upload

---
+++ C# Fundamentals/03.Arrays/00.StringArray-to-Chars-to-Strings-to-int-Array/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace KaminoFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string inputString = Console.ReadLine();
+            string inputSequence = string.Empty;
+            while (inputString != "Clone them!")
+            {
+                for (int i = 0; i < inputString.Length; i++)
+                {
+                    char ch = inputString[i];
+                    if (ch == '!')
+                    {
+                        ch = ' ';
+                    }
+                    string letter = ch.ToString();
+                    inputSequence += letter;
+                }
+                int[] inputArray = inputSequence.Split().Select(int.Parse).ToArray();
+            }
+        }
+    }
+}
+                
