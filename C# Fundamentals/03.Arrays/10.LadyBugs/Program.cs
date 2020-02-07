3f63d2861fc0dc21275cec61804d8c0651013d55
RADO <53035214+RadoTech@users.noreply.github.com>
2020-02-07 17:28:51 +0200

    LadyBugs***
    10.	*LadyBugs
    You are given a field size and the indexes of ladybugs inside the field. After that on every new line until the "end" command is given, a ladybug changes its position either to its left or to its right by a given fly length. 
    A command to a ladybug looks like this: "0 right 1". This means that the little insect placed on index 0 should fly one index to its right. If the ladybug lands on a fellow ladybug, it continues to fly in the same direction by the same fly length. If the ladybug flies out of the field, it is gone.
    For example, imagine you are given a field with size 3 and ladybugs on indexes 0 and 1. If the ladybug on index 0 needs to fly to its right by the length of 1 (0 right 1) it will attempt to land on index 1 but as there is another ladybug there it will continue further to the right by additional length of 1, landing on index 2. After that, if the same ladybug needs to fly to its right by the length of 1 (2 right 1), it will land somewhere outside of the field, so it flies away:
     
    If you are given ladybug index that does not have ladybug there, nothing happens. If you are given ladybug index that is outside the field, nothing happens. 
    Your job is to create the program, simulating the ladybugs flying around doing nothing. At the end, print all cells in the field separated by blank spaces. For each cell that has a ladybug on it print '1' and for each empty cells print '0'. For the example above, the output should be '0 1 0'. 
    Input
    •	On the first line you will receive an integer - the size of the field
    •	On the second line you will receive the initial indexes of all ladybugs separated by a blank space. The given indexes may or may not be inside the field range
    •	On the next lines, until you get the "end" command you will receive commands in the format: "{ladybug index} {direction} {fly length}"
    Output
    •	Print the all cells within the field in format: "{cell} {cell} … {cell}"
    o	If a cell has ladybug in it, print '1'
    o	If a cell is empty, print '0' 
    Constrains
    •	The size of the field will be in the range [0 … 1000]
    •	The ladybug indexes will be in the range [-2,147,483,647 … 2,147,483,647]
    •	The number of commands will be in the range [0 … 100] 
    •	The fly length will be in the range [-2,147,483,647 … 2,147,483,647]
    Examples:
    Input
    3
    0 1
    0 right 1
    2 right 1
    end
    Output
    0 1 0
    Comments
    1 1 0 - Initial field
    0 1 1 - field after "0 right 1"
    0 1 0 - field after "2 right 1"
    Input
    3
    0 1 2
    0 right 1
    1 right 1
    2 right 1
    end
    Output
    0 0 0
    Input
    5
    3
    3 left 2
    1 left -2
    end
    Output
    0 0 0 1 0

---
+++ C# Fundamentals/03.Arrays/10.LadyBugs/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Linq;
+
+namespace LadyBugs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fieldSize = int.Parse(Console.ReadLine()); //input
+            int[] indexes = Console.ReadLine()
+                            .Split()
+                            .Select(int.Parse)
+                            .ToArray();
+
+            int[] initialField = new int[fieldSize]; //creating field for lady bugs
+            for (int i = 0; i < indexes.Length; i++)
+            {
+                if (indexes[i] < initialField.Length
+                    && indexes[i] >= 0)
+                {
+                    initialField[indexes[i]] = 1; //defining where are lady bugs initially
+                }
+            }
+
+            string input = Console.ReadLine();
+            string[] command = new string[3];
+
+            while (input != "end")
+            {
+                command = input.Split().ToArray();
+                int ladyBug = int.Parse(command[0]); //command separation on 3 parts
+                string direction = command[1];
+                int flyLength = int.Parse(command[2]);
+
+                if (ladyBug < initialField.Length //check position is in the field
+                    && ladyBug >= 0
+                    && initialField[ladyBug] == 1 //and if there is a lady bug
+                    && flyLength != 0) //and if it's moving at all
+                {
+                    if (direction == "right") //if lady bug is moving to the right
+                    {
+                        if (ladyBug + flyLength < initialField.Length //if it's moving in the field
+                            && ladyBug + flyLength >= 0                
+                            && initialField[ladyBug + flyLength] == 0) //and if the place to go is free;
+                        {
+                            initialField[ladyBug + flyLength] = 1; //if yes, this place is being taken
+                        }
+                        else if (ladyBug + flyLength < initialField.Length
+                                 && ladyBug + flyLength >= 0
+                                 && initialField[ladyBug + flyLength] == 1) //if the place to go is NOT FREE
+                        {
+                            while (ladyBug + flyLength < initialField.Length
+                                   && ladyBug + flyLength >= 0
+                                   && initialField[ladyBug + flyLength] == 1) //moving forward with same step count until finds a free place.
+                            {
+                                flyLength += flyLength; //that's why we are increasing the step with the same amount;
+                                if (ladyBug + flyLength < initialField.Length
+                                    && ladyBug + flyLength >= 0 // check for negative number which will cause the lady bug to step out left of the field;
+                                    && initialField[ladyBug + flyLength] == 0)
+                                {
+                                    initialField[ladyBug + flyLength] = 1; //if free place found, will be taken
+                                    break; //and step out of the loop
+                                }
+                            }
+                        }
+                    }
+                    else if (direction == "left") //same logic for moving left
+                    {
+                        if (ladyBug - flyLength >= 0 // the moving here is with minus
+                            && ladyBug - flyLength < initialField.Length
+                            && initialField[ladyBug - flyLength] == 0)
+                        {
+                            initialField[ladyBug - flyLength] = 1;
+                        }
+                        else if (ladyBug - flyLength >= 0
+                                 && ladyBug - flyLength < initialField.Length
+                                 && initialField[ladyBug - flyLength] == 1)
+                        {
+                            while (ladyBug - flyLength >= 0 // until (while loop) finds a free place
+                                   && ladyBug - flyLength < initialField.Length
+                                   && initialField[ladyBug - flyLength] == 1)
+                            {
+                                flyLength += flyLength; // keeping plus
+                                if (ladyBug - flyLength >= 0
+                                    && ladyBug - flyLength < initialField.Length
+                                    && initialField[ladyBug - flyLength] == 0)
+                                {
+                                    initialField[ladyBug - flyLength] = 1;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+
+                    initialField[ladyBug] = 0; //if the code enters the very first if statement = ladybug has taken of its initial position.
+                }
+
+                input = Console.ReadLine(); // reading again the very first while loop.
+            }
+
+            Console.WriteLine(String.Join(" ", initialField)); //Output after all loops are over.
+        }
+    }
+}
