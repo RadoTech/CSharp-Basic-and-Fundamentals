14ba1464c584ac425b0621ad2dc2c9a3454d285c
RADO <53035214+RadoTech@users.noreply.github.com>
2020-02-14 23:21:22 +0200

    Commit 01. Distance Calculator
    Create a program that calculates what percentage you’ve travelled. First, you will receive how many steps you’ve made. Then, you will receive how long 1 step is in centimeters. Last, you will receive the distance you need to travel in meters.
    Then you have to calculate what distance you have travelled with the steps given. Have in mind that every fifth step is 30% shorter than usual. You have to calculate what percentage of the distance you’ve travelled.
    In the end, print the percentage of the distance travelled, formatted to the 2nd decimal place, in the following format:
    "You travelled {percentage}% of the distance!"
    Input
    •	On the 1st line you will receive the steps made – an integer number in the range [0…100000]
    •	On the 2nd line you will receive the length of 1 step – a real number in the range [0.0…50.0]
    •	On the 3rd line you will receive the distance you need to travel – an integer number in the range [0…100000]
    Output
    •	In the end print the percentage of the distance travelled formatted to the 2nd decimal place in the format described above.
    Constraints
    •	The input will always be in the right format.
    •	Percentage can be over 100%.
    Examples
    Input
    100
    2
    1
    Output
    You travelled 188.00% of the distance!
    Comments
    The length of a step is 2 centimeters. Every fifth step will be 1.4 centimeters long. 20 shorter steps are made. The distance that has to be travelled is 1 meter. The distance travelled is 1.88 meters which is 188% of the distance that had to be travelled.
    Input
    5000
    7.5
    500
    Output
    You travelled 70.50% of the distance!

---
+++ C# Fundamentals/Mid-Exam-Preparation/30-June-2019-Group-1/01.Distance-Calculator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DistanceCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input:
+            int stepsMade = int.Parse(Console.ReadLine());//in cm.
+            double stepLength = double.Parse(Console.ReadLine()); //in cm.
+            int distanceToTravel = int.Parse(Console.ReadLine());//in m.
+            //Logic                                                     
+            double allStepsLen = ShortSteps(stepsMade, stepLength) + NormalSteps(stepsMade, stepLength);
+            double percentDistanceTraveled = ((allStepsLen / 100) / distanceToTravel) * 100;
+            Console.WriteLine($"You travelled {percentDistanceTraveled:f2}% of the distance!");
+        }
+
+        static double NormalSteps(int stepsMade, double stepLength)
+        {
+            int normalStepsMade = stepsMade - (stepsMade / 5);
+            double normalStepsLength = normalStepsMade * stepLength;
+            return normalStepsLength;
+        }
+        static double ShortSteps(int stepsMade, double stepLength)
+        {
+            int shortStepsMade = stepsMade / 5;
+            double shortStepsLength = shortStepsMade * (stepLength * 0.7);
+            return shortStepsLength;
+        }
+    }
+}
