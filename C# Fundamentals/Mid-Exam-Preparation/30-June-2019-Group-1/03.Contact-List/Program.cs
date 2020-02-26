9320bce061c6b6953e53848283dc060748812d34
RADO <53035214+RadoTech@users.noreply.github.com>
2020-02-26 09:31:19 +0200

    Initial commit 03. Contact List
    Create a program that helps you keep track of the contacts that you have. You will receive the list of contacts you already have on a single line, separated by a single space in the following format:
    "{contact1} {contact2} {contact3}… {contactn}"
    Then you will receive commands that you need to execute over your list. There are four possible commands:
    •	"Add {contact} {index}":
    o	If {contact} isn’t already contained – add it in the end of the collection.
    o	If {contact} is already contained – add it on the given index, if the index exists.
    •	"Remove {index}"
    o	Remove the contact on the given index, if the index exists.
    •	"Export {startIndex} {count}":
    o	Print the next {count} contacts starting from the given {startIndex} (including), separated by a single space. If the count requested is more than the contacts- just print them to the end. 
    "{contact} {contact} {contact}"
    •	"Print Normal/Reversed"
    o	Print the contact list in normal (in the order they have been added) or reversed order and then stop the program:
    "Contacts: {contact1} {contact2}… {contactn}"
    Input
    •	On the 1st line, you will receive the starting list with the names of the contacts separated by a single space.
    •	On the next lines, you will receive commands in the format described above.
    Output
    •	Print the needed output upon the "Export" command.
    •	Print the list after the manipulations upon the "Print" command in the format described above.
     

---
+++ C# Fundamentals/Mid-Exam-Preparation/30-June-2019-Group-1/03.Contact-List/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ContactList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> InputList = new List<string>(Console.ReadLine().Split());
+            var InputCommand = new List<string>(Console.ReadLine().Split());
+
+            while (!InputCommand[0].Contains("Print"))
+            {
+                AddContact(InputList, InputCommand);
+                RemoveContact(InputList, InputCommand);
+                ExportContact(InputList, InputCommand);
+                InputCommand = new List<string>(Console.ReadLine().Split());
+            }
+            PrintContact(InputList, InputCommand);
+
+        }
+
+        private static void PrintContact(List<string> InputList, List<string> InputCommand)
+        {
+            if (InputCommand[0].Contains("Print"))
+            {
+                if (InputCommand[1].Contains("Normal"))
+                {
+                    Console.WriteLine($"Contacts: {String.Join(" ", InputList)}");
+                }
+                else if (InputCommand[1].Contains("Reversed"))
+                {
+                    InputList.Reverse();
+                    Console.WriteLine($"Contacts: {String.Join(" ", InputList)}");
+                }
+
+            }
+        }
+
+        public static List<string> AddContact(List<string> InputList, List<string> InputCommand)
+        {
+            if (InputCommand[0].Contains("Add"))
+            {
+                if (InputList.Contains(InputCommand[1]))
+                {
+                    InputList.Insert(int.Parse(InputCommand[2]), InputCommand[1]);
+                }
+                else if (!InputList.Contains(InputCommand[1]))
+                {
+                    InputList.Add(InputCommand[1]);
+                }
+            }
+            return InputList;
+        }
+        public static List<string> RemoveContact(List<string> InputList, List<string> InputCommand)
+        {
+            if (InputCommand[0].Contains("Remove"))
+            {
+                for (int index = 0; index < InputList.Count; index++)
+                {
+                    if (index == int.Parse(InputCommand[1]))
+                    {
+                        InputList.RemoveAt(index);
+                    }
+                }
+            }
+            return InputList;
+        }
+        public static void ExportContact(List<string> InputList, List<string> InputCommand)
+        {
+            if (InputCommand[0].Contains("Export"))
+            {
+                List<string> ExportList = new List<string>();
+                for (int index = 0; index < InputList.Count; index++)
+                {
+                    int count = (int.Parse(InputCommand[1]) + int.Parse(InputCommand[2])) - 1;
+                    if (int.Parse(InputCommand[1]) >= 0 && count < (InputList.Count -1)) //InputCommand[2] - here is count not index
+                    {
+                        if (index >= int.Parse(InputCommand[1]) && index <= count)
+                        {
+                            ExportList.Add(InputList[index]);
+                        }
+
+                    }
+                    else if (int.Parse(InputCommand[1]) >= 0 && count >= (InputList.Count - 1)) //InputCommand[2] - here is count not index
+                    {
+                        if (index >= int.Parse(InputCommand[1]) && index < InputList.Count)
+                        {
+                            ExportList.Add(InputList[index]);
+                        }
+                    }
+                }
+                Console.WriteLine(String.Join(" ", ExportList));
+            }
+        }
+    }
+}
