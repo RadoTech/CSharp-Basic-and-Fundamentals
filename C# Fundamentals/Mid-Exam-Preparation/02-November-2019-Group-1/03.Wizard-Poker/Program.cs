d798e5642994cd93fd347f6a0dc4506ab25d33c9
RADO <53035214+RadoTech@users.noreply.github.com>
2020-02-28 14:01:49 +0200

    Commit 03.Wizzard Poker
    3. Wizard Poker
    The world is threatened by an enemy never seen before. Your hero's weapon seems to be useless against the enemy. But your hero has a super strong arsenal full of powerful magic cards and will challenge the enemy to a card duel to the death and he needs your help to create a deck. 
    Create a program that adds, inserts, removes and swaps cards in a new deck. On the first line, you will receive all cards in the form of strings separated by ":". Until you receive the "Ready" command, you will get commands in the format:
    •	Add {card name}
    •	Adds the card to the end of the deck.
    •	If the card doesn't exist in print "Card not found."
    •	Insert {card name} {index}
    •	Insert the card at the given index of the deck.
    •	If the card doesn't exist or the index is invalid print "Error!"
    •	Remove {card name} 
    •	Remove the card from the deck.
    •	If the card doesn't exist in print "Card not found."
    •	Swap {card name 1} {card name 2} 
    •	Swap the positions of the cards. 
    •	Input will always be valid
    •	Shuffle deck 
    •	Reverse the deck
    When you receive the "Ready" command print the cards in the deck separated by space.
    Input
    •	On the 1st line, you will receive the arsenal of all cards available separated by ":".
    •	On the next lines, until you receive the "Ready" command you will receive commands to arrange your deck.
    Output
    •	Print the cards in your deck on a single line, separated by a single space.
    Examples
    Input
    Innervate:Moonfire:Pounce:Claw:Wrath:Bite
    Add Moonfire
    Add Pounce
    Add Bite
    Add Wrath
    Insert Claw 0
    Swap Claw Moonfire
    Remove Bite
    Shuffle deck
    Ready
    Output
    Wrath Pounce Claw Moonfire
    Comments
    First command is Add Moofire and now our deck has one card in it.
    1. Moonfire Pounce
    2. Moonfire Pounce Bite
    3. Moonfire Pounce Bite Wrath
    4. Claw Moonfire Pounce Bite Wrath
    5. Moonfire Claw Pounce Bite Wrath
    6. Moonfire Claw Pounce Wrath
    7. Wrath Pounce Claw Moonfire
    
    Input
    Wrath:Pounce:Lifeweaver:Exodia:Aso:Pop
    Add Pop
    Add Exodia
    Add Aso
    Remove Wrath
    Add SineokBqlDrakon
    Shuffle deck
    Insert Pesho 0
    Ready
    Output
    Card not found.
    Card not found.
    Error!
    Aso Exodia Pop

---
+++ C# Fundamentals/Mid-Exam-Preparation/02-November-2019-Group-1/03.Wizard-Poker/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WizzardDeck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> cards = Console.ReadLine().Split(':').ToList();
+            string input = Console.ReadLine();
+            List<string> hand = new List<string>();
+
+            while (input != "Ready")
+            {
+                string[] splitted = input.Split();
+                switch (splitted[0])
+                {
+                    case "Add":
+                        {
+                            if (cards.Contains(splitted[1]))
+                            {
+                                hand.Add(splitted[1]);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Card not found.");
+                            }
+                        };
+                        break;
+                    case "Insert":
+                        {
+                            string cardName = splitted[1];
+                            int index = int.Parse(splitted[2]);
+                            if (index < 0 || index > hand.Count || cards.IndexOf(cardName) == -1)
+                            {
+                                Console.WriteLine("Error!");
+                            }
+                            else
+                            {
+                                hand.Insert(index, cardName);
+                            }
+                          
+                        };
+                        break;
+                    case "Remove":
+                        {
+                            if (hand.Contains(splitted[1]))
+                            {
+                                hand.Remove(splitted[1]);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Card not found.");
+                            }
+                        };
+                        break;
+                    case "Swap":
+                        {
+                            int first = hand.IndexOf(splitted[1]);
+                            int second = hand.IndexOf(splitted[2]);
+                            string temp = hand[first];
+                            hand[first] = hand[second];
+                            hand[second] = temp;
+                        };
+                        break;
+                    case "Shuffle":
+                        {
+                            hand.Reverse();
+                        };
+                        break;
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(String.Join(" ", hand));
+        }
+    }
+}
