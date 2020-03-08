caaed2eb5a18e3f834f74ecf18177d9173326c0b
RADO <53035214+RadoTech@users.noreply.github.com>
2020-03-08 23:35:44 +0200

    Commit A Miner Task
    You will be given a sequence of strings, each on a new line. Every odd line on the console is representing a resource (e.g. Gold, Silver, Copper, and so on) and every even - quantity. Your task is to collect the resources and print them each on a new line.
    Print the resources and their quantities in the following format:
    {resource} –> {quantity}
    The quantities will be in the range [1 … 2 000 000 000]
    Examples
    Input
    gold
    155
    silver
    10
    copper
    17
    gold
    15
    stop
    Output
    gold -> 170
    silver -> 10
    copper -> 17

---
+++ C# Fundamentals/07.Associative-Arrays/02.A-Miner-Task/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AMinerTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputOre = Console.ReadLine();
+            ulong inputVolume = ulong.Parse(Console.ReadLine());
+            Dictionary<string, ulong> minerProduction = new Dictionary<string, ulong>();
+            while (inputOre != "stop")
+            {
+                if (!minerProduction.ContainsKey(inputOre))
+                {
+                    minerProduction.Add(inputOre, inputVolume);
+                }
+                else
+                {
+                    minerProduction[inputOre] += inputVolume;
+                }
+                inputOre = Console.ReadLine();
+                if (inputOre == "stop")
+                {
+                    break;
+                }
+                inputVolume = ulong.Parse(Console.ReadLine());
+            }
+            foreach (var ore in minerProduction)
+            {
+                Console.WriteLine($"{ore.Key} -> {ore.Value}");
+            }
+        }
+    }
+}
