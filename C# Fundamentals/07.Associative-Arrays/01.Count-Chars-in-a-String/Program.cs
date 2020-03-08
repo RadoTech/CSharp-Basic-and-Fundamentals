edbaaf9a2461e5f5dfb69e3ea4fa8067a76ae740
RADO <53035214+RadoTech@users.noreply.github.com>
2020-03-08 22:18:33 +0200

    Commit 01.Count Chars in a String
    Write a program that counts all characters in a string except for space (' '). 
    Print all the occurrences in the following format:
    {char} -> {occurrences}
    Examples
    Input
    text
    Output
    t -> 2
    e -> 1
    x -> 1
    Input
    text text text
    Output
    t -> 6
    e -> 3
    x -> 3

---
+++ C# Fundamentals/07.Associative-Arrays/01.Count-Chars-in-a-String/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CountCharsInAString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char, uint> histogram = new Dictionary<char, uint>();
+            string input = Console.ReadLine();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] != ' ')
+                {
+                    if (!histogram.ContainsKey(input[i]))
+                    {
+                        histogram.Add(input[i], 1);
+                    }
+                    else
+                    {
+                        histogram[input[i]]++;
+                    }
+                }
+            }
+            foreach (var KeyValuePair in histogram)
+            {
+                Console.WriteLine($"{KeyValuePair.Key} -> {KeyValuePair.Value}");
+            }
+        }
+    }
+}
