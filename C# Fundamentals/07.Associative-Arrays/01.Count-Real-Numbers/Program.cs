76ece81754b7e7dcde69e2d287eac2d05012791c
RADO <53035214+RadoTech@users.noreply.github.com>
2022-01-14 16:29:15 +0200

    Add files via upload

---
+++ C# Fundamentals/07.Associative-Arrays/01.Count-Real-Numbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace CountRealNumbers_Dictionaries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] inputNumbers = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+            SortedDictionary<double, int> repeatsDict = new SortedDictionary<double, int>();
+
+            for (int i = 0; i < inputNumbers.Length; i++)
+            {
+                if (!repeatsDict.ContainsKey(inputNumbers[i]))
+                {
+                    repeatsDict.Add(inputNumbers[i], 1);
+                }
+                else
+                {
+                    repeatsDict[inputNumbers[i]]++;
+                }
+            }
+            foreach (var item in repeatsDict)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
